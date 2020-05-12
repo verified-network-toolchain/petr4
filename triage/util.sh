8dd595cdeffd8008a0add793dcc2b0dfcfd311f5
Ryan Doenges <me@ryandoeng.es>
2020-05-11 22:16:21 -0400

    add missing failures to failures.csv, sort the file

--- triage/util.sh
+++ triage/util.sh
@@ -5,3 +5,5 @@ alias stfnow="make test-stf 2>&1 > testruns/stfnow.txt; cat testruns/stfnow.txt 
 alias stfold="make test-stf 2>&1 > testruns/stfold.txt; cat testruns/stfold.txt | grep FAIL | sed -E 's/.* +//' | sort | uniq > testruns/stffailsold.txt"                                                                                                
 alias chkold="make test 2>&1 | cat > testruns/testresultsold.txt; cat testruns/testresultsold.txt | grep FAIL | sed -E 's/.* +//' | sort | uniq > testruns/failsold.txt"
 alias chknow="make test 2>&1 | cat > testruns/testresultsnow.txt; cat testruns/testresultsnow.txt | grep FAIL | sed -E 's/.* +//' | sort | uniq > testruns/failsnow.txt"
+
+alias canonicalize="sort -t ',' -k1,1 -k4,4n triage/failures.csv -o triage/failures.csv"

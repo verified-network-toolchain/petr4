47dc1b6f3e58f9c8306ed2e422675dc9e21c8e17
Tobias Kappé <tobias@kap.pe>
2020-10-22 11:38:59 -0400

    Extraction of headers and records.

--- lib/Platform/Packet.v
+++ lib/Platform/Packet.v
@@ -30,7 +30,22 @@ Fixpoint read_first_bits (count: nat) : packet_monad (Bvector count) :=
       end
   end.
 
-Definition eval_packet_extract_fixed (into: value) : packet_monad value :=
+Fixpoint eval_packet_extract_fixed (into: value) : packet_monad value :=
+  let eval_packet_extract_fixed_multiple :=
+    (* This needs to be an inline fixpoint; if we split it out, Coq cannot guess the decreasing argument. *)
+    fix f (fs: MStr.Raw.t value)
+      : packet_monad (MStr.Raw.t value) :=
+      match fs with
+      | nil => mret nil
+      | (k, v) :: tail =>
+        let* inner := eval_packet_extract_fixed v in
+        let* rest := f tail
+        in mret ((k, inner) :: rest)
+      end in
+  let eval_packet_extract_header (hdr: header) : packet_monad header :=
+    let 'MkHeader _ fs := hdr in
+    let* fs' := eval_packet_extract_fixed_multiple fs in
+    mret (MkHeader true fs') in
   match into with
   | ValBool _ =>
     let* vec := read_first_bits 1 in
@@ -50,5 +65,11 @@ Definition eval_packet_extract_fixed (into: value) : packet_monad value :=
       mret (ValFixedInt width negated)
     | _ => state_fail Internal
     end
+  | ValRecord fs =>
+    let* fs' := eval_packet_extract_fixed_multiple fs in
+    mret (ValRecord fs')
+  | ValHeader hdr =>
+    let* hdr' := eval_packet_extract_header hdr in
+    mret (ValHeader hdr)
   | _ => state_fail Internal
   end.

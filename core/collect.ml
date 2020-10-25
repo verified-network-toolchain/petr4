ea88655dd84972e8d2c282253a17fe392fe7cfca
rnp39 <rnp39@cornell.edu>
2020-10-24 20:28:45 -0700

    gather and collect now recursive

--- core/collect.ml
+++ core/collect.ml
@@ -7,7 +7,7 @@ module Info = I
 (** Collects all error and match-kind declarations.
   INVARIANT: The output program contains no
     match-kind nor error declarations. *)
-let collect
+let rec collect
   (Program p) : Ast.id list * Ast.match_kind list * program =
   p
   |> List.fold_left
@@ -17,13 +17,23 @@ let collect
             let open Declaration in
             acc |>
             match d with
-            | (_, Error {members})     -> Tuple.T3.map_fst
+            | _, Error {members}     -> Tuple.T3.map_fst
               ~f:begin fun errors ->
                 List.map ~f:snd members @ errors end
-            | (_, MatchKind {members}) -> Tuple.T3.map_snd
+            | _, MatchKind {members} -> Tuple.T3.map_snd
               ~f:begin fun match_kinds ->
                 List.map ~f:(Ast.mk $$ snd) members @ match_kinds end
-            | _                        -> Tuple.T3.map_trd
+            | i, Parser prsr         ->
+              fun (errors, match_kinds, rev_prog) ->
+                let pes, pmks, Program lcls = collect (Program prsr.locals) in
+                pes @ errors, pmks @ match_kinds,
+                (i, Parser { prsr with locals = lcls}) :: rev_prog
+            | i, Control ctrl        ->
+              fun (errors, match_kinds, rev_prog) ->
+                let ces, cmks, Program lcls = collect (Program ctrl.locals) in
+                ces @ errors, cmks @ match_kinds,
+                (i, Control { ctrl with locals = lcls }) :: rev_prog
+            | _                      -> Tuple.T3.map_trd
               ~f:begin fun rev_prog -> d :: rev_prog end
           end
       ~init:([],[],[])

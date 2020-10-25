e28380b53ad1f1773a0734d721d60c27ff4acff8
rnp39 <rnp39@cornell.edu>
2020-10-24 17:12:36 -0700

    collecting errors and matchkinds

--- core/collect.ml
+++ core/collect.ml
@@ -1,3 +1,28 @@
-(** Collects all error and match-kind declarations  *)
+module I = Petr4.Info
+open Petr4.Prog
+open Core_kernel
+open Util
+module Info = I
 
-(* TODO *)
+(** Collects all error and match-kind declarations  *)
+let collect
+  (Program p) : Ast.id list * Ast.match_kind list * program =
+  p
+  |> List.fold_left
+      ~f:begin fun
+          (acc :Ast.id list * Ast.match_kind list * Declaration.t list)
+          (d : Declaration.t) ->
+            let open Declaration in
+            acc |>
+            match d with
+            | (_, Error {members})     -> Tuple.T3.map_fst
+              ~f:begin fun errors ->
+                List.map ~f:snd members @ errors end
+            | (_, MatchKind {members}) -> Tuple.T3.map_snd
+              ~f:begin fun match_kinds ->
+                List.map ~f:(Ast.mk $$ snd) members @ match_kinds end
+            | _ -> Tuple.T3.map_trd 
+              ~f:begin fun rev_prog -> d :: rev_prog end
+          end
+      ~init:([],[],[])
+  |> Tuple.T3.map_trd ~f:begin fun p -> Program (List.rev p) end

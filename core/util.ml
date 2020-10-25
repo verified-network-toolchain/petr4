107d22378b7c0aec9d149be5592511ad69ddffba
rnp39 <rnp39@cornell.edu>
2020-10-24 19:11:17 -0700

    began gathering functions to prepare for inlining

--- core/util.ml
+++ core/util.ml
@@ -1,3 +1,6 @@
 (** Function composition. *)
-let ($$) (f : 'b -> 'c) (g : 'a -> 'b) =
+let ($$) (f : 'b -> 'c) (g : 'a -> 'b) : 'a -> 'c =
  fun (x : 'a) -> x |> g |> f
+
+(** Lifting declaration lists to programs. *)
+let pgm (ds : Petr4.Prog.Declaration.t list) : Petr4.Prog.program = Petr4.Prog.Program ds

45c2ab704c7ad1b9a6bf09b849ac665d4573fb59
rnp39 <rnp39@cornell.edu>
2020-11-24 21:30:55 -0800

    Coq's Notation parsing forces me to use a lisp style for binops

--- core/Check.v
+++ core/Check.v
@@ -51,8 +51,60 @@ Module CheckExpr (LOC INT BIGINT NAME : P4Data).
 
   (** Expression typing as a relation. *)
   Inductive check (g : gam) (d : del) : e -> t -> dir -> Prop :=
+    | chk_bool (b : bool) :
+        g, d |= BOOL b \in Bool \goes DIn
     | chk_int (n : INT.t) :
         g, d |=  Int n \in int \goes DIn
-    where "gm ',' dl '|=' exp '\in' typ '\goes' drc"
+    | chk_bitstring (w : INT.t) (v : BIGINT.t) :
+        g, d |= w @ v \in bit<w> \goes DIn
+    | chk_var (x : NAME.t) (ty : t) :
+        g x = Some ty ->
+        g, d |= Var x :: ty end \in ty \goes DInOut
+   | chk_not (exp : e) :
+       g, d |= exp \in Bool \goes DIn ->
+       g, d |= ! exp :: Bool end \in Bool \goes DIn
+   | chk_bitnot (w : INT.t) (exp : e) :
+       g, d |= exp \in bit<w> \goes DIn ->
+       g, d |= ~ exp :: bit<w> end \in bit<w> \goes DIn
+   | chk_uminus (exp : e) :
+       g, d |= exp \in int \goes DIn ->
+       g, d |= - exp :: int end \in int \goes DIn
+   | chk_plus (e1 e2 : e) :
+       g, d |= e1 \in int \goes DIn ->
+       g, d |= e2 \in int \goes DIn ->
+       g, d |= + e1 :: int e2 :: int end \in int \goes DIn
+   | chk_minus (e1 e2 : e) :
+       g, d |= e1 \in int \goes DIn ->
+       g, d |= e2 \in int \goes DIn ->
+       g, d |= -- e1 :: int e2 :: int end \in int \goes DIn
+   | chk_plussat (n : INT.t) (e1 e2 : e) :
+       g, d |= e1 \in bit<n> \goes DIn ->
+       g, d |= e2 \in bit<n> \goes DIn ->
+       g, d |= |+| e1 :: bit<n> e2 :: bit<n> end \in bit<n> \goes DIn
+   | chk_minussat (n : INT.t) (e1 e2 : e) :
+       g, d |= e1 \in bit<n> \goes DIn ->
+       g, d |= e2 \in bit<n> \goes DIn ->
+       g, d |= |-| e1 :: bit<n> e2 :: bit<n> end \in bit<n> \goes DIn
+   | chk_bitand (n : INT.t) (e1 e2 : e) :
+       g, d |= e1 \in bit<n> \goes DIn ->
+       g, d |= e2 \in bit<n> \goes DIn ->
+       g, d |= & e1 :: bit<n> e2 :: bit<n> end \in bit<n> \goes DIn
+   | chk_bitor (n : INT.t) (e1 e2 : e) :
+       g, d |= e1 \in bit<n> \goes DIn ->
+       g, d |= e2 \in bit<n> \goes DIn ->
+       g, d |= | e1 :: bit<n> e2 :: bit<n> end \in bit<n> \goes DIn
+   | chk_bitxor (n : INT.t) (e1 e2 : e) :
+       g, d |= e1 \in bit<n> \goes DIn ->
+       g, d |= e2 \in bit<n> \goes DIn ->
+       g, d |= ^ e1 :: bit<n> e2 :: bit<n> end \in bit<n> \goes DIn
+   | chk_and (e1 e2 : e) :
+       g, d |= e1 \in Bool \goes DIn ->
+       g, d |= e2 \in Bool \goes DIn ->
+       g, d |= && e1 :: Bool e2 :: Bool end \in Bool \goes DIn
+   | chk_or (e1 e2 : e) :
+       g, d |= e1 \in Bool \goes DIn ->
+       g, d |= e2 \in Bool \goes DIn ->
+       g, d |= || e1 :: Bool e2 :: Bool end \in Bool \goes DIn
+   where "gm ',' dl '|=' exp '\in' typ '\goes' drc"
       := (check gm dl exp typ drc).
 End CheckExpr.

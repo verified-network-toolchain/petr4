52da27ef63d9c6dc21158be625d78da36c9709fa
rnp39 <rnp39@cornell.edu>
2020-11-28 16:57:19 -0800

    added some stmt notations, refactored notation organization

--- core/Check.v
+++ core/Check.v
@@ -33,8 +33,9 @@ Module CheckExpr (LOC NAME : P4Data) (INT BIGINT : P4Numeric).
   Module IU := P4NumericUtil(INT).
   Infix "+" := IU.add (at level 50, left associativity).
 
-  Module EXPR := Expr LOC NAME INT BIGINT.
-  Export EXPR.
+  Module E := Expr LOC NAME INT BIGINT.
+  Module F := E.F.
+  Export E.ExprNotations.
 
   Module NM := Env NAME.
   Module LM := Env LOC.
@@ -46,14 +47,14 @@ Module CheckExpr (LOC NAME : P4Data) (INT BIGINT : P4Numeric).
   Definition matchkinds : Type := NM.env unit.
 
   (** Typing context. *)
-  Definition gam : Type := NM.env t.
+  Definition gam : Type := NM.env E.t.
 
   Reserved Notation "'$' ers ',,' mks ',,' gm '$' '|=' ex '\in' ty '\goes' dr"
            (at level 40, ex custom p4expr, ty custom p4type at level 0).
 
   (** Expression typing as a relation. *)
   Inductive check (errs : errors) (mkds : matchkinds)
-            (g : gam) : dir -> e -> t -> Prop :=
+            (g : gam) : dir -> E.e -> E.t -> Prop :=
     (* Literals. *)
     | chk_bool (b : bool) :
         $ errs ,, mkds ,, g $ |= BOOL b \in Bool \goes DIn
@@ -61,127 +62,127 @@ Module CheckExpr (LOC NAME : P4Data) (INT BIGINT : P4Numeric).
         $ errs ,, mkds ,, g $ |=  Int n \in int \goes DIn
     | chk_bitstring (w : INT.t) (v : BIGINT.t) :
         $ errs ,, mkds ,, g $ |= w @ v \in bit<w> \goes DIn
-    | chk_var (x : NAME.t) (ty : t) :
+    | chk_var (x : NAME.t) (ty : E.t) :
         g x = Some ty ->
         $ errs ,, mkds ,, g $ |= Var x :: ty end \in ty \goes DInOut
    (* Unary operations. *)
-   | chk_not (exp : e) :
+   | chk_not (exp : E.e) :
        $ errs ,, mkds ,, g $ |= exp \in Bool \goes DIn ->
        $ errs ,, mkds ,, g $ |= ! exp :: Bool end \in Bool \goes DIn
-   | chk_bitnot (w : INT.t) (exp : e) :
+   | chk_bitnot (w : INT.t) (exp : E.e) :
        $ errs ,, mkds ,, g $ |= exp \in bit<w> \goes DIn ->
        $ errs ,, mkds ,, g $ |= ~ exp :: bit<w> end \in bit<w> \goes DIn
-   | chk_uminus (exp : e) :
+   | chk_uminus (exp : E.e) :
        $ errs ,, mkds ,, g $ |= exp \in int \goes DIn ->
        $ errs ,, mkds ,, g $ |= - exp :: int end \in int \goes DIn
    (* Binary Operations. *)
-   | chk_plus (e1 e2 : e) :
+   | chk_plus (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in int \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in int \goes DIn ->
        $ errs ,, mkds ,, g $
          |= + e1 :: int e2 :: int end \in int \goes DIn
-   | chk_minus (e1 e2 : e) :
+   | chk_minus (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in int \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in int \goes DIn ->
        $ errs ,, mkds ,, g $
          |= -- e1 :: int e2 :: int end \in int \goes DIn
-   | chk_plussat (n : INT.t) (e1 e2 : e) :
+   | chk_plussat (n : INT.t) (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in bit<n> \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in bit<n> \goes DIn ->
        $ errs ,, mkds ,, g $
          |= |+| e1 :: bit<n> e2 :: bit<n> end \in bit<n> \goes DIn
-   | chk_minussat (n : INT.t) (e1 e2 : e) :
+   | chk_minussat (n : INT.t) (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in bit<n> \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in bit<n> \goes DIn ->
        $ errs ,, mkds ,, g $
          |= |-| e1 :: bit<n> e2 :: bit<n> end \in bit<n> \goes DIn
-   | chk_bitand (n : INT.t) (e1 e2 : e) :
+   | chk_bitand (n : INT.t) (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in bit<n> \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in bit<n> \goes DIn ->
        $ errs ,, mkds ,, g $
          |= & e1 :: bit<n> e2 :: bit<n> end \in bit<n> \goes DIn
-   | chk_bitor (n : INT.t) (e1 e2 : e) :
+   | chk_bitor (n : INT.t) (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in bit<n> \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in bit<n> \goes DIn ->
        $ errs ,, mkds ,, g $
             |= | e1 :: bit<n> e2 :: bit<n> end \in bit<n> \goes DIn
-   | chk_bitxor (n : INT.t) (e1 e2 : e) :
+   | chk_bitxor (n : INT.t) (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in bit<n> \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in bit<n> \goes DIn ->
        $ errs ,, mkds ,, g $
          |= ^ e1 :: bit<n> e2 :: bit<n> end \in bit<n> \goes DIn
-   | chk_and (e1 e2 : e) :
+   | chk_and (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in Bool \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in Bool \goes DIn ->
        $ errs ,, mkds ,, g $
             |= && e1 :: Bool e2 :: Bool end \in Bool \goes DIn
-   | chk_or (e1 e2 : e) :
+   | chk_or (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in Bool \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in Bool \goes DIn ->
        $ errs ,, mkds ,, g $
          |= || e1 :: Bool e2 :: Bool end \in Bool \goes DIn
-   | chk_le (e1 e2 : e) :
+   | chk_le (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in int \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in int \goes DIn ->
        $ errs ,, mkds ,, g $
             |= <= e1 :: int e2 :: int end \in Bool \goes DIn
-   | chk_ge (e1 e2 : e) :
+   | chk_ge (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in int \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in int \goes DIn ->
        $ errs ,, mkds ,, g $
          |= >= e1 :: int e2 :: int end \in Bool \goes DIn
-   | chk_lt (e1 e2 : e) :
+   | chk_lt (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in int \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in int \goes DIn ->
        $ errs ,, mkds ,, g $
          |= < e1 :: int e2 :: int end \in Bool \goes DIn
-   | chk_gt (e1 e2 : e) :
+   | chk_gt (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in int \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in int \goes DIn ->
        $ errs ,, mkds ,, g $
          |= > e1 :: int e2 :: int end \in Bool \goes DIn
-   | chk_eq (ty : t) (e1 e2 : e) :
+   | chk_eq (ty : E.t) (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in ty \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in ty \goes DIn ->
        $ errs ,, mkds ,, g $
          |= == e1 :: ty e2 :: ty end \in Bool \goes DIn
-   | chk_neq (ty : t) (e1 e2 : e) :
+   | chk_neq (ty : E.t) (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in ty \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in ty \goes DIn ->
        $ errs ,, mkds ,, g $
          |= != e1 :: ty e2 :: ty end \in Bool \goes DIn
-   | chk_shl (n : INT.t) (e1 e2 : e) :
+   | chk_shl (n : INT.t) (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in bit<n> \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in int \goes DIn ->
        $ errs ,, mkds ,, g $
          |= << e1 :: bit<n> e2 :: int end \in bit<n> \goes DIn
-   | chk_shr (n : INT.t) (e1 e2 : e) :
+   | chk_shr (n : INT.t) (e1 e2 : E.e) :
        $ errs ,, mkds ,, g $ |= e1 \in bit<n> \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in int \goes DIn ->
        $ errs ,, mkds ,, g $
          |= >> e1 :: bit<n> e2 :: int end \in bit<n> \goes DIn
-   | chk_plusplus (m n w : INT.t) (e1 e2 : e) :
+   | chk_plusplus (m n w : INT.t) (e1 e2 : E.e) :
        m + n = w ->
        $ errs ,, mkds ,, g $ |= e1 \in bit<m> \goes DIn ->
        $ errs ,, mkds ,, g $ |= e2 \in bit<n> \goes DIn ->
        $ errs ,, mkds ,, g $
          |= ++ e1 :: bit<m> e2 :: bit<n> end \in bit<w> \goes DIn
    (* Member expressions. *)
-   | chk_hdr_mem (he : e) (x : NAME.t)
-                 (fields : fs t) (tx : t) (dr : dir) :
+   | chk_hdr_mem (he : E.e) (x : NAME.t)
+                 (fields : F.fs E.t) (tx : E.t) (dr : dir) :
        In (x, tx) fields ->
        $ errs ,, mkds ,, g $ |= he \in hdr { fields } \goes dr ->
        $ errs ,, mkds ,, g $
          |= [ he :: hdr { fields } ] x \in tx \goes dr
-   | chk_rec_mem (re : e) (x : NAME.t)
-                 (fields : fs t) (tx : t) (dr : dir) :
+   | chk_rec_mem (re : E.e) (x : NAME.t)
+                 (fields : F.fs E.t) (tx : E.t) (dr : dir) :
        In (x, tx) fields ->
        $ errs ,, mkds ,, g $ |= re \in rec { fields } \goes dr ->
        $ errs ,, mkds ,, g $
          |= [ re :: rec { fields } ] x \in tx \goes dr
    (* Records. *)
-   | chk_rec_lit (efs : fs e) (tfs : fs t) :
-      relfs (check errs mkds g DIn) efs tfs ->
+   | chk_rec_lit (efs : F.fs E.e) (tfs : F.fs E.t) :
+      F.relfs (check errs mkds g DIn) efs tfs ->
       $ errs ,, mkds ,, g $ |= rec { efs } \in rec { tfs } \goes DIn
    (* Errors and matchkinds. *)
    | chk_error (err : NAME.t) :
@@ -191,10 +192,11 @@ Module CheckExpr (LOC NAME : P4Data) (INT BIGINT : P4Numeric).
        mkds mkd = Some tt ->
        $ errs ,, mkds ,, g $ |= Matchkind mkd \in error \goes DIn
    (* Action and extern calls. TODO: directions. *)
-   | chk_call (params : fs t) (args : fs e) (returns : t) (callee : e) :
+   | chk_call (params : F.fs E.t) (args : F.fs E.e)
+              (returns : E.t) (callee : E.e) :
        $ errs ,, mkds ,, g $
          |= callee \in params |-> returns \goes DIn ->
-       relfs (check errs mkds g DIn) args params ->
+       F.relfs (check errs mkds g DIn) args params ->
        $ errs ,, mkds ,, g $
          |= call callee :: {{ params |-> returns }} with args end
          \in returns \goes DIn

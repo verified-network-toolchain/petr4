80bd2e3b16cdbe09dd6a9adc1bbd3d7169e85fdc
rnp39 <rnp39@cornell.edu>
2021-01-13 16:19:21 -0800

    decidable equivalence class instances for String, P4Int, and P4String

--- deps/poulet4/lib/P4String.v
+++ deps/poulet4/lib/P4String.v
@@ -1,4 +1,5 @@
 Require Petr4.String.
+Require Import Coq.Classes.EquivDec.
 
 Record t (tags_t: Type) :=
   { tags: tags_t;
@@ -18,5 +19,21 @@ Definition equivb [tags_t: Type] (s1 s2: t tags_t) :=
 Definition eq_const [tags_t: Type] (s1: t tags_t) (s2: String.t) :=
   String.eqb s1.(str) s2.
 
+(* TODO: this is going bye bye. *)
 Definition make {tags_t : Type} (tgs : tags_t) (x : String.t) :=
   {| tags := tgs; str := x |}.
+
+Instance EquivEquivalence (tags_t : Type) : Equivalence (@equiv tags_t).
+Proof.
+  constructor.
+  - constructor.
+  - intros [] [] H; unfold equiv in *; simpl in *; subst; auto.
+  - intros [] [] [] H1 H2; unfold equiv in *; simpl in *; subst; auto.
+Defined.
+
+Instance P4StringEqDec (tags_t : Type) : EqDec (t tags_t) (@equiv tags_t).
+Proof.
+  intros [t1 s1] [t2 s2]. Locate "===".
+  unfold Equivalence.equiv; unfold complement; simpl; unfold equiv; simpl.
+  apply equiv_dec.
+Defined.

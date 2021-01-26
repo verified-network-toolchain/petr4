fbd5167d99e58c86fde79dbd59f0775eb5dd7f80
Ryan Doenges <me@ryandoeng.es>
2021-01-26 10:42:32 -0500

    add hand-compiled c to examples/c_tests

---
+++ examples/c_tests/petr4-runtime.h
@@ -0,0 +1,17 @@
+// Need to expose GMP operations for each P4 construct
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef char bool;
+#define true 1
+#define false 0
+
+typedef char bit;
+
+typedef void* packet_in;
+typedef void* packet_out;
+
+void extract(packet_in pkt, void* data, int len);
+void emit(packet_out pkt, void* data, int len);
---
+++ examples/c_tests/simple.c
@@ -0,0 +1,55 @@
+#include "petr4-runtime.h"
+
+typedef struct simple_h {
+  bool valid;
+  bit src;
+  bit dst;
+} simple_h;
+typedef struct headers {
+  simple_h simple;
+} headers;
+typedef struct P_state {
+  packet_in pkt;
+  headers hdrs;
+} P_state;
+void P(P_state* state) {
+  extract(state->pkt, &state->hdrs.simple, 16);
+}
+typedef struct C_state {
+  headers hdrs;
+  bool forward;
+} C_state;
+void C_forward(C_state* state) {
+  state->forward = true;
+}
+void C_drop(C_state* state) {
+  state->forward = false;
+}
+void C(C_state* state) {
+  if(state->hdrs.simple.dst != 0) {
+    C_forward(state);
+  } else { 
+    C_drop(state);
+  }
+}
+typedef struct D_state {
+  packet_out pkt;
+  headers hdrs;
+} D_state;
+void D(D_state* state) {
+    emit(state->pkt, &state->hdrs.simple, 16);
+}
+int main() {
+  P_state p;
+  C_state c;
+  D_state d;
+  p.pkt = "Hi";
+  P(&p);
+  c.hdrs = p.hdrs;
+  C(&c);
+  d.hdrs = c.hdrs;
+  d.pkt = malloc(2);
+  D(&d);
+  printf("Out: %s\nForward: %d\n", d.pkt, c.forward);
+  return 0;
+}

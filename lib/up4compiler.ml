59e579cfedc843301879b891aefa163add31fdc5
Calvin-S <19calvins@alumni.tas.tw>
2021-01-27 13:53:48 -0500

    added standard_meta_param

--- lib/up4compiler.ml
+++ lib/up4compiler.ml
@@ -1,26 +1,6 @@
 exception IncorrectType of string
 module P4 = Types
 
-let prog_merge (prog1 : P4.program) (prog2 : P4.program) : P4.program = prog1
-
-let parser_merge (p1 : P4.Declaration.t) (p2 : P4.Declaration.t) : P4.Declaration.t = p1
-
-
-let if_BareName : P4.name = P4.BareName (Info.M "Created Condition", "condition")
-
-let cond_If : P4.Statement.t =
-  (Info.M "Created If", P4.Statement.Conditional {
-      cond = Info.M "Created Condition", P4.Expression.Name (if_BareName);
-      tru = Info.M "Created Return", P4.Statement.Return {expr = None};
-      fls = None})
-
-let cond_param (info: Info.t) : P4.Parameter.t = let open P4.Parameter in 
-  (info, { annotations = [];
-           direction = Some (Info.M "Created param", P4.Direction.In);
-           typ = (Info.M "Created param", P4.Type.Bool);
-           variable = (Info.M "Created param", "condition");
-           opt_value = None})
-
 (** Record for temp control *)
 type tempControl = { annotations: P4.Annotation.t list;
                      name: P4.P4String.t;
@@ -39,7 +19,28 @@ let get_control (d : P4.Declaration.t) : tempControl = match (snd d) with
       constructor_params = constructor_params; 
       locals = locals; 
       apply = apply }
-  | _ -> raise (IncorrectType "Control type expected in deparser_merge")
+  | _ -> raise (IncorrectType "Control type expected")
+
+let standard_meta : P4.Type.t = Info.M "Standard Meta", P4.Type.TypeName (P4.BareName (Info.M "Standard Meta", "standard_metadata_t"))
+
+let standard_meta_param (info: Info.t) : P4.Parameter.t = let open P4.Parameter in 
+  (info, { annotations = [];
+           direction = Some (Info.M "Created param", P4.Direction.InOut);
+           typ = standard_meta;
+           variable = (Info.M "Created param", "standard_metadata");
+           opt_value = None})
+
+let if_BareName : P4.name = P4.BareName (Info.M "Created Condition", "condition")
+
+let cond_if : P4.Statement.t =
+  (Info.M "Created If", P4.Statement.Conditional {
+      cond = Info.M "Created Condition", P4.Expression.Name (if_BareName);
+      tru = Info.M "Created Return", P4.Statement.Return {expr = None};
+      fls = None})
+
+let add_cond_if (b1 : P4.Block.t) : P4.Block.t = let open P4.Block in 
+  (fst b1, { annotations = (snd b1).annotations;
+             statements = cond_if :: (snd b1).statements})
 
 let block_merge (b1 : P4.Block.t) (b2 : P4.Block.t) : P4.Block.t = 
   let open P4.Block in
@@ -57,3 +58,27 @@ let deparser_merge (d1 : P4.Declaration.t) (d2: P4.Declaration.t) : P4.Declarati
                        locals = dp1.locals @ dp2.locals;
                        apply = block_merge dp1.apply dp2.apply})
 
+let match_declaration (d : P4.Declaration.t) (mainProg: bool) : (P4.Declaration.t option) = 
+  match (snd d) with
+  | Parser { annotations=a; name=n; type_params=tp; params=p; constructor_params=cp; locals=l; states=s} -> 
+    Some ((fst d), P4.Declaration.Parser {
+        annotations = a;
+        name = n;
+        type_params = tp;
+        params = [standard_meta_param (p |> List.hd |> fst)];
+        constructor_params = cp;
+        locals = l;
+        states = s
+      })
+  | P4.Declaration.Control { annotations=a; name=n; type_params=tp; params=p; constructor_params=cp; locals=l; apply=app} -> 
+    if mainProg then Some ((fst d), P4.Declaration.Control {
+        annotations = a;
+        name = n;
+        type_params = tp;
+        params = [standard_meta_param (p |> List.hd |> fst)];
+        constructor_params = cp;
+        locals = l;
+        apply = add_cond_if app }) else None
+  | _ -> None
+
+let prog_merge (prog1 : P4.program) (prog2 : P4.program) : P4.program = prog1

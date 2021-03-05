0db48e12708fa3066f3e54214cf56d210089ca0b
John Sarracino <jsarraci@cs.ucsd.edu>
2021-03-05 13:41:46 -0800

    rework Hoare triples to include error case

--- deps/coq-poulet4/lib/Monads/Hoare.v
+++ deps/coq-poulet4/lib/Monads/Hoare.v
@@ -10,17 +10,15 @@ Definition Pred {State: Type} := State -> Prop.
 Definition top {State: Type} : @Pred State := fun _ => True.
 Hint Unfold top : core.
 
-(* TODO: I think we can make this total by changing Q to take (Result + Exception) as second argument *)
 Definition hoare_triple_partial
     {State Exception Result: Type}
     {P: @Pred State}
     {c: @state_monad State Exception Result}
-    {Q: Result -> State -> @Pred State} : Prop := 
+    {Q: Result + Exception -> State -> @Pred State} : Prop := 
   forall st, P st -> 
-    match c st with 
-    | (inr _, _)    => True
-    | (inl v, st')  => Q v st st'
-    end.
+    let (v, st') := c st in
+      Q v st st'
+    .
 
 Notation "{{ P }} c {{ Q }}" := (@hoare_triple_partial _ _ _ P c Q) (at level 90) : hoare_scope.
 Ltac gregsimp := 
@@ -41,7 +39,7 @@ Ltac gregsimp :=
               end) ; subst ; simpl in * ; try firstorder ; auto with arith.
 
 Lemma hoare_consequence {State Exception Result: Type} :
-  forall (P P': @Pred State) (Q Q': Result -> State -> @Pred State) (c: @state_monad State Exception Result),
+  forall (P P': @Pred State) (Q Q': Result + Exception -> State -> @Pred State) (c: @state_monad State Exception Result),
   (forall i, P' i -> P i) ->
   (forall v i f, Q v i f -> Q' v i f) ->
   {{ P }} c {{ Q }} ->
@@ -55,27 +53,24 @@ Proof.
   specialize (H H2).
   specialize (H1 H).
   destruct (c st).
-  destruct s.
-    -   
-      specialize (H0 r st s0).
-      auto.
-    - auto.
+  specialize (H0 s st s0).
+  auto.
 Qed.
 
 Lemma hoare_return {State Exception Result: Type} (x: Result) : 
-  {{ top }} @state_return State Exception Result x {{ fun r st st' => r = x /\ st = st' }}.
+  {{ top }} @state_return State Exception Result x {{ fun r st st' => r = inl x /\ st = st' }}.
 Proof.
   cbv. intros. auto.
 Qed.
 
-Lemma hoare_fail {State Exception Result: Type} : 
-  forall (P: @Pred State) (Q: Result -> State -> @Pred State) (exc: Exception), {{ P }} state_fail exc {{ Q }}.
+Lemma hoare_fail {State Exception Result: Type}  (exc: Exception) : 
+  {{ top }} @state_fail State Exception Result exc {{ fun r st st' => r = inr exc /\ st = st' }}.
 Proof.
   cbv. intros. auto.
 Qed.
 
 Lemma hoare_get {State Exception: Type} :
-  {{ top }} @get_state State Exception {{ fun r s s' => s = s' /\ r = s }}.
+  {{ top }} @get_state State Exception {{ fun r s s' => s = s' /\ r = inl s }}.
 Proof.
   cbv. intros. auto.
 Qed.
@@ -86,18 +81,21 @@ Proof.
   cbv. intros. auto.
 Qed.
 
+
 Lemma hoare_bind
   {State A B Exception: Type}
   {P: @Pred State}
-  {P': A -> @Pred State}
-  {Q: A -> State -> @Pred State}
-  {Q': A -> B -> State -> @Pred State}
+  {P': A + Exception -> @Pred State}
+  {Q: A + Exception -> State -> @Pred State}
+  {Q': A + Exception -> B + Exception -> State -> @Pred State}
   {c: @state_monad State Exception A}
   {f: A -> @state_monad State Exception B}
   :
   {{ P }} c {{ Q }} ->
-  (forall r: A, {{ P' r }} (f r) {{ Q' r}}) ->
-  {{ fun st_init => P st_init /\ forall (r: A) (st: State), Q r st_init st -> P' r st}}
+  (forall r: A, {{ P' (inl r) }} (f r) {{ Q' (inl r)}}) ->
+  (forall (e: Exception) (st: State), P' (inr e) st -> Q' (inr e) (inr e) st st) ->
+    (* (forall (e: Exception) (st: State), P st -> Q (inr e) st st -> *)
+  {{ fun st_init => P st_init /\ forall (r: A + Exception) (st: State), Q r st_init st -> P' r st}}
   c >>= f
   {{ fun r st_init st_final => exists a st_middle, Q a st_init st_middle /\ Q' a r st_middle st_final }}.
 Proof.
@@ -106,15 +104,25 @@ Proof.
   specialize (H st).
   induction (c st).
   induction a.
-  specialize (H0 a b).
-  destruct H1 as [H1 H2].
-  specialize (H2 a b).
-  induction (f a b).
-  induction a0.
-  -
-    exists a.
+  
+  2 : {
+    destruct H2 as [H2 H3].
+    exists (inr b0).
     exists b.
-    auto.
+    split.
+    - auto.
+    -
+      specialize (H3 (inr b0) b).
+      specialize (H1 b0 b).
+      auto.
+  }
+  specialize (H0 a b).
+  destruct H2 as [H2 H3].
+  specialize (H3 (inl a) b).
+  induction (f a b).
+  exists (inl a).
+  exists b.
+  split.
   - auto.
   - auto.
 Qed.

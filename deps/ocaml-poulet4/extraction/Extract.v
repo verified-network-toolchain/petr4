717e8f74e20853c5352e27f5058fce49abf0c8c6
Shengyi Wang <txyyss@gmail.com>
2021-03-13 23:47:02 +0800

    Add code to export Trans.v to OCaml.

--- deps/ocaml-poulet4/extraction/Extract.v
+++ deps/ocaml-poulet4/extraction/Extract.v
@@ -4,8 +4,15 @@ Require Import Coq.extraction.ExtrOcamlNatInt.
 Require Import Petr4.ExtractOcamlBinNumsBigint.
 Require Petr4.Syntax.
 Require Petr4.Typed.
+Require Petr4.Trans.
 Require Extraction.
 
+Extract Constant Trans.to_digit => "(fun x -> Char.chr 20)".
+Extract Constant Trans.to_N_aux => "(fun x y z -> z)".
+Extract Constant Trans.N_to_string => "(fun x -> Z.to_string (Bigint.to_zarith_bigint x))".
+Extract Constant Trans.add1 => "(fun x -> Bigint.of_zarith_bigint (Z.succ (Bigint.to_zarith_bigint x)))".
+Extract Inlined Constant Trans.Nzero => "(Bigint.of_zarith_bigint Z.zero)".
+
 Cd "lib".
-Separate Extraction Syntax Typed.
+Separate Extraction Syntax Typed Trans.
 Cd "..".

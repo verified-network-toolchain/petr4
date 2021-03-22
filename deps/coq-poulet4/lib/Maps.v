26d8c76ba8f1990d8bd3c45d59d6dac07d6b7b61
Shengyi Wang <txyyss@gmail.com>
2021-03-22 19:09:36 +0800

    Unify IdentMap and PathMap to a single implementation: FuncAsMap.

--- deps/coq-poulet4/lib/Maps.v
+++ deps/coq-poulet4/lib/Maps.v
@@ -3,6 +3,33 @@ Require Import Coq.Bool.Bool.
 Require P4String.
 Import ListNotations.
 
+Module FuncAsMap.
+
+  Section FuncAsMap.
+
+    Context {key: Type}.
+    Context {key_eqb: key -> key -> bool}.
+    Context {value: Type}.
+
+    Definition t := key -> option value.
+
+    Definition empty: t := fun _ => None.
+    Definition get: key -> t -> option value := fun k fmap => fmap k.
+    Definition set: key -> value -> t -> t :=
+      fun k v fmap x => if key_eqb x k then Some v else fmap x.
+    
+    Definition sets: list key -> list value -> t -> t :=
+      fun kList vList fmap =>
+        fold_left (fun fM kvPair => set (fst kvPair) (snd kvPair) fM)
+                  (combine kList vList) fmap.
+    
+    Definition gets (kl: list key) (m: t): list (option value) :=
+      map (fun k => get k m) kl.
+    
+  End FuncAsMap.
+
+End FuncAsMap.
+
 Module IdentMap.
 
 Section IdentMap.
@@ -11,20 +38,14 @@ Context {tags_t: Type}.
 Notation ident := (P4String.t tags_t).
 Context {A: Type}.
 
-Definition t := ident -> option A.
-
-Definition empty : t := fun _ => None.
-Definition get : ident -> t -> option A := fun id idMap => idMap id.
+Definition t := @FuncAsMap.t ident A.
+Definition empty : t := FuncAsMap.empty.
+Definition get : ident -> t -> option A := FuncAsMap.get.
 Definition set : ident -> A -> t -> t :=
-  fun id value idMap x => if P4String.equivb x id then Some value else idMap x.
-
+  @FuncAsMap.set ident (@P4String.equivb tags_t) A.
 Definition sets: list ident -> list A -> t -> t :=
-  fun idList valueList idMap =>
-    fold_left (fun idM ivPair => set (fst ivPair) (snd ivPair) idM)
-              (combine idList valueList) idMap.
-
-Definition gets (kl : list ident) (m : t) : list (option A) :=
-  map (fun k => get k m) kl.
+  @FuncAsMap.sets ident (@P4String.equivb tags_t) A.
+Definition gets: list ident -> t -> list (option A) := FuncAsMap.gets.
 
 End IdentMap.
 
@@ -47,20 +68,14 @@ Notation ident := (P4String.t tags_t).
 Notation path := (list ident).
 Context {A: Type}.
 
-Definition t := path -> option A.
-
-Definition empty : t := fun _ => None.
-Definition get : path -> t -> option A := fun p pM => pM p.
+Definition t := @FuncAsMap.t path A.
+Definition empty : t := FuncAsMap.empty.
+Definition get : path -> t -> option A := FuncAsMap.get.
 Definition set : path -> A -> t -> t :=
-  fun p v pM x => if path_equivb x p then Some v else pM x.
-
+  @FuncAsMap.set path path_equivb A.
 Definition sets : list path -> list A -> t -> t :=
-  fun pList vList pMap =>
-    fold_left (fun idM ivPair => set (fst ivPair) (snd ivPair) idM)
-              (combine pList vList) pMap.
-
-Definition gets (kl : list path) (m : t) : list (option A) :=
-  map (fun k => get k m) kl.
+  @FuncAsMap.sets path path_equivb A.
+Definition gets: list path -> t -> list (option A) := FuncAsMap.gets.
 
 End PathMap.
 

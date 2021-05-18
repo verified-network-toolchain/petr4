2e39faae3d0e28d264e633eb6536406d36f6a9d5
Tobias Kappé <tobias@kap.pe>
2021-05-18 15:02:25 -0400

    Extend pre-bisimulations with up-to operators.

--- deps/poulet4/lib/P4automata/PreBisimulation.v
+++ deps/poulet4/lib/P4automata/PreBisimulation.v
@@ -16,149 +16,6 @@ Definition chunked_relation
   )
 .
 
-Definition buffer_appended
-  {a: p4automaton}
-  (c c': configuration a)
-  (b: bool)
-  : Prop
-:=
-  let '(s, st, buf) := c in
-  let '(s', st', buf') := c' in
-  buf' = buf ++ b :: nil /\
-  s = s' /\
-  st = st'
-.
-
-Definition buffer_filled
-  {a: p4automaton}
-  (c c': configuration a)
-  (b: bool)
-  : Prop
-:=
-  let '(s, st, buf) := c in
-  let '(s', st', buf') := c' in
-  match s with
-  | inl s =>
-    Datatypes.length buf + 1 = size a s /\
-    buf' = nil /\
-    st' = update a s (buf ++ b :: nil) st /\
-    s' = transitions a s st'
-  | inr _ => False
-  end
-.
-
-Definition buffer_sane
-  {a: p4automaton}
-  (c: configuration a)
-  : Prop
-:=
-  let '(s, st, buf) := c in
-  match s with
-  | inl s => Datatypes.length buf < size a s
-  | inr _ => True
-  end
-.
-
-Definition build_chunk
-  {a1 a2: p4automaton}
-  (R: configuration a1 -> configuration a2 -> Prop)
-  (c1: configuration a1)
-  (c2: configuration a2)
-:=
-  R c1 c2 /\
-  buffer_sane c1 /\
-  buffer_sane c2
-.
-
-Definition symbolic_step
-  {a1 a2: p4automaton}
-  (R: configuration a1 -> configuration a2 -> Prop)
-  : chunked_relation a1 a2
-:=
-  (* First case: neither buffer was filled. *)
-  build_chunk (fun c1' c2' =>
-     exists c1 c2 b,
-       R c1 c2 /\
-       buffer_appended c1 c1' b /\
-       buffer_appended c2 c2' b
-  ) ::
-  (* Second case: the left buffer was filled, but the right was not. *)
-  build_chunk (fun c1' c2' =>
-     exists c1 c2 b,
-       R c1 c2 /\
-       buffer_filled c1 c1' b /\
-       buffer_appended c2 c2' b
-  ) ::
-  (* Third case: the right buffer was filled, but the left was not. *)
-  build_chunk (fun c1' c2' =>
-     exists c1 c2 b,
-       R c1 c2 /\
-       buffer_appended c1 c1' b /\
-       buffer_filled c2 c2' b
-  ) ::
-  (* Fourth case: both buffers were filled. *)
-  build_chunk (fun c1' c2' =>
-     exists c1 c2 b,
-       R c1 c2 /\
-       buffer_filled c1 c1' b /\
-       buffer_filled c2 c2' b
-  ) ::
-  nil
-.
-
-Lemma buffer_sane_preserved
-  {a: p4automaton}
-  (c: configuration a)
-  (b: bool)
-:
-  buffer_sane c ->
-  buffer_sane (step c b)
-.
-Proof.
-  unfold buffer_sane; intros.
-  destruct c as ((s, st), buf).
-  destruct s; auto.
-  simpl step.
-  destruct (equiv_dec _ _).
-  destruct (transitions _ _ _); auto.
-  - simpl length.
-    apply a.
-  - rewrite app_length in *.
-    unfold "===", complement in *.
-    simpl length in *.
-    lia.
-Qed.
-
-Lemma appended_or_filled
-  {a: p4automaton}
-  (c: configuration a)
-  (b: bool)
-:
-  buffer_sane c ->
-  buffer_appended c (step c b) b \/
-  buffer_filled c (step c b) b
-.
-Proof.
-  intros.
-  destruct c as ((s, st), buf).
-  destruct s.
-  - simpl step.
-    rewrite app_length.
-    simpl length.
-    destruct (equiv_dec (length buf + 1) (size a s)).
-    + right.
-      unfold buffer_filled.
-      easy.
-    + left.
-      simpl.
-      intros.
-      easy.
-  - left.
-    simpl step.
-    unfold buffer_appended.
-    easy.
-Qed.
-
 Inductive chunked_related
   {a1 a2: p4automaton}
   : chunked_relation a1 a2 ->
@@ -169,8 +26,6 @@ Inductive chunked_related
 | ChunkedRelatedHead:
     forall c1 c2 (R: configuration a1 -> configuration a2 -> Prop) rel,
       R c1 c2 ->
-      buffer_sane c1 ->
-      buffer_sane c2 ->
       chunked_related (R :: rel) c1 c2
 | ChunkedRelatedTail:
     forall c1 c2 R rel,
@@ -185,9 +40,7 @@ Lemma chunked_related_correct
   (c2: configuration a2)
 :
   chunked_related R c1 c2 <->
-  (buffer_sane c1 /\
-   buffer_sane c2 /\
-   exists R', In R' R /\ R' c1 c2)
+  exists R', In R' R /\ R' c1 c2
 .
 Proof.
   split; intros.
@@ -199,8 +52,8 @@ Proof.
     + firstorder.
   - induction R.
     + firstorder.
-    + destruct H as [? [? [R' [? ?]]]].
-      destruct H1.
+    + destruct H as [R' [? ?]].
+      destruct H.
       * apply ChunkedRelatedHead; auto.
         congruence.
       * apply ChunkedRelatedTail.
@@ -226,51 +79,56 @@ Proof.
   firstorder.
 Qed.
 
-Lemma symbolic_step_correct
-  {a1 a2: p4automaton}
-  (R: configuration a1 -> configuration a2 -> Prop)
-  (c1: configuration a1)
-  (c2: configuration a2)
-  (b: bool)
-:
-  buffer_sane c1 ->
-  buffer_sane c2 ->
-  R c1 c2 ->
-  chunked_related (symbolic_step R) (step c1 b) (step c2 b)
+Definition rel (a1 a2: p4automaton) :=
+  configuration a1 -> configuration a2 -> Prop
 .
-Proof.
-  intros.
-  unfold symbolic_step.
-  destruct (appended_or_filled c1 b H),
-           (appended_or_filled c2 b H0).
-  - apply ChunkedRelatedHead.
-    unfold build_chunk; repeat split.
-    exists c1, c2, b; easy.
-    all: eauto using buffer_sane_preserved.
-  - do 2 apply ChunkedRelatedTail; apply ChunkedRelatedHead.
-    unfold build_chunk; repeat split.
-    exists c1, c2, b; easy.
-    all: eauto using buffer_sane_preserved.
-  - do 1 apply ChunkedRelatedTail; apply ChunkedRelatedHead.
-    unfold build_chunk; repeat split.
-    exists c1, c2, b; easy.
-    all: eauto using buffer_sane_preserved.
-  - do 3 apply ChunkedRelatedTail; apply ChunkedRelatedHead.
-    unfold build_chunk; repeat split.
-    exists c1, c2, b; easy.
-    all: eauto using buffer_sane_preserved.
-Qed.
+
+Definition closure_extends
+  {a1 a2: p4automaton}
+  (close: rel a1 a2 -> rel a1 a2)
+:=
+  forall (R: rel a1 a2) c1 c2,
+    R c1 c2 -> close R c1 c2
+.
+
+Definition closure_preserves_accept
+  {a1 a2: p4automaton}
+  (close: rel a1 a2 -> rel a1 a2)
+:=
+  forall (R: rel a1 a2),
+    (forall c1 c2, R c1 c2 -> accepting c1 <-> accepting c2) ->
+    (forall c1 c2, close R c1 c2 -> accepting c1 <-> accepting c2)
+.
+
+Definition closure_preserves_transition
+  {a1 a2: p4automaton}
+  (close: rel a1 a2 -> rel a1 a2)
+:=
+  forall (R: rel a1 a2),
+    (forall c1 c2, R c1 c2 ->
+     forall b, close R (step c1 b) (step c2 b)) ->
+    (forall c1 c2, close R c1 c2 ->
+     forall b, close R (step c1 b) (step c2 b))
+.
+
+Definition closure_mono
+  {a1 a2: p4automaton}
+  (close: rel a1 a2 -> rel a1 a2)
+:=
+  forall (R R': rel a1 a2),
+    (forall c1 c2, R c1 c2 -> R' c1 c2) ->
+    (forall c1 c2, close R c1 c2 -> close R' c1 c2)
+.
 
 Definition progresses
   {a1 a2: p4automaton}
+  (close: rel a1 a2 -> rel a1 a2)
   (expanded: chunked_relation a1 a2)
   (front: chunked_relation a1 a2)
 :=
   forall c1 c2 b,
     chunked_related expanded c1 c2 ->
-    chunked_related (front ++ expanded)
-                    (step c1 b)
-                    (step c2 b)
+    close (chunked_related (front ++ expanded)) (step c1 b) (step c2 b)
 .
 
 Definition acceptance_ok
@@ -284,75 +142,61 @@ Definition acceptance_ok
 
 Definition pre_bisimulation
   {a1 a2: p4automaton}
+  (close: rel a1 a2 -> rel a1 a2)
   (expanded: chunked_relation a1 a2)
   (front: chunked_relation a1 a2)
 :=
   acceptance_ok expanded ->
-  progresses expanded front ->
+  progresses close expanded front ->
   forall c1 c2,
-    chunked_related (front ++ expanded) c1 c2 ->
+    close (chunked_related (front ++ expanded)) c1 c2 ->
     bisimilar c1 c2
 .
+
 
 Lemma pre_bisimulation_intro
   {a1 a2: p4automaton}
+  (close: rel a1 a2 -> rel a1 a2)
   (R: configuration a1 -> configuration a2 -> Prop)
 :
-  pre_bisimulation nil (R :: nil) ->
-  (forall c1 c2,
-    R c1 c2 ->
-    buffer_sane c1 ->
-    buffer_sane c2 ->
-    bisimilar c1 c2)
+  closure_extends close ->
+  pre_bisimulation close nil (R :: nil) ->
+  (forall c1 c2, R c1 c2 -> bisimilar c1 c2)
 .
 Proof.
   intros.
-  apply H.
+  apply H0.
   - intros c1' c2' ?.
-    inversion H3.
+    inversion H2.
   - intros c1' c2' ? ?.
-    inversion H3.
+    inversion H2.
   - rewrite app_nil_r.
-    now constructor.
-Qed.
-
-Definition grows_to_bisimulation
-  {a1 a2: p4automaton}
-  (R: configuration a1 -> configuration a2 -> Prop)
-:=
-  forall c1 c2,
-    build_chunk R c1 c2 ->
-    bisimilar c1 c2
-.
-
-Lemma pre_bisimulation_intro'
-  {a1 a2: p4automaton}
-  (R: configuration a1 -> configuration a2 -> Prop)
-:
-  pre_bisimulation nil (R :: nil) ->
-  grows_to_bisimulation R
-.
-Proof.
-  intros.
-  unfold grows_to_bisimulation.
-  intros.
-  destruct H0 as [? [? ?]].
-  revert c1 c2 H0 H1 H2.
-  now apply pre_bisimulation_intro.
+    apply H.
+    now apply ChunkedRelatedHead.
 Qed.
 
 Lemma pre_bisimulation_leaf
   {a1 a2: p4automaton}
+  (close: rel a1 a2 -> rel a1 a2)
   (checked: chunked_relation a1 a2)
 :
-  pre_bisimulation checked nil
+  closure_preserves_accept close ->
+  closure_preserves_transition close ->
+  pre_bisimulation close checked nil
 .
 Proof.
-  unfold pre_bisimulation.
-  intros.
+  intros ? ? ? ? ? ? ?.
   rewrite app_nil_l in *.
-  exists (chunked_related checked).
-  firstorder.
+  exists (close (chunked_related checked)).
+  split; auto.
+  intros c1' c2' ?; split.
+  - revert c1' c2' H4.
+    apply H.
+    apply H1.
+  - revert c1' c2' H4.
+    apply H0.
+    intros.
+    now apply H2.
 Qed.
 
 Ltac solve_incl :=
@@ -380,81 +224,180 @@ Ltac solve_incl :=
   end
 .
 
+Definition symbolic_step
+  {a1 a2: p4automaton}
+  (R: configuration a1 -> configuration a2 -> Prop)
+  (c1: configuration a1)
+  (c2: configuration a2)
+  : Prop
+:=
+  exists c1' c2' b,
+    R c1' c2' /\ step c1' b = c1 /\ step c2' b = c2
+.
+
 Lemma pre_bisimulation_grow
   {a1 a2: p4automaton}
+  (close: rel a1 a2 -> rel a1 a2)
   (checked: chunked_relation a1 a2)
   (front: chunked_relation a1 a2)
   (R: configuration a1 -> configuration a2 -> Prop)
 :
+  closure_mono close ->
+  closure_extends close ->
+  closure_preserves_accept close ->
   (forall c1 c2, R c1 c2 -> accepting c1 <-> accepting c2) ->
-  pre_bisimulation (R :: checked) (symbolic_step R ++ front) ->
-  pre_bisimulation checked (R :: front)
+  pre_bisimulation close (R :: checked) (symbolic_step R :: front) ->
+  pre_bisimulation close checked (R :: front)
 .
 Proof.
-  intros; unfold pre_bisimulation; intros.
-  apply H0.
+  intros ? ? ? ? ? ? ? ? ? ?.
+  apply H3.
   - intros c1' c2' ?.
-    inversion H4; subst; auto.
+    inversion H7; subst; auto.
   - intros c1' c2' ? ?.
-    inversion H4; subst.
-    + apply symbolic_step_correct with (b0 := b) in H7; auto.
-      apply chunked_related_subset with (R1 := symbolic_step R); auto.
+    inversion_clear H7; subst.
+    + apply H0.
+      rewrite <- app_comm_cons.
+      apply ChunkedRelatedHead.
+      exists c1', c2', b.
+      easy.
+    + apply (H5 _ _ b) in H8.
+      eapply H in H8.
+      exact H8.
+      intros.
+      apply chunked_related_subset with (R1 := (R :: front) ++ checked); auto.
       solve_incl.
-    + apply chunked_related_subset with (R1 := (R :: front) ++ checked); auto.
-      solve_incl.
-  - apply chunked_related_subset with ((R :: front) ++ checked); auto.
+  - revert c1 c2 H6.
+    apply H.
+    intros.
+    apply chunked_related_subset with (R1 := (R :: front) ++ checked); auto.
     solve_incl.
 Qed.
 
 Lemma pre_bisimulation_replace
   {a1 a2: p4automaton}
+  (close: rel a1 a2 -> rel a1 a2)
   (R R': configuration a1 -> configuration a2 -> Prop)
   (checked front: chunked_relation a1 a2)
 :
+  closure_mono close ->
   (forall c1 c2, R c1 c2 -> R' c1 c2) ->
-  pre_bisimulation checked (R' :: front) ->
-  pre_bisimulation checked (R :: front)
+  pre_bisimulation close checked (R' :: front) ->
+  pre_bisimulation close checked (R :: front)
 .
 Proof.
   unfold pre_bisimulation; intros.
-  apply H0; auto.
+  apply H1; auto.
   - intros c1' c2' b ?.
-    apply (H2 _ _ b) in H4.
+    apply (H3 _ _ b) in H5.
     rewrite <- app_comm_cons in *.
-    inversion H4; subst.
-    + apply H in H7.
-      now apply ChunkedRelatedHead.
+    eapply H in H5.
+    exact H5.
+    intros.
+    inversion H6; subst.
+    + now apply ChunkedRelatedHead, H0.
     + now apply ChunkedRelatedTail.
   - rewrite <- app_comm_cons in *.
-    inversion H3; subst.
-    + apply H in H6.
-      now apply ChunkedRelatedHead.
+    eapply H in H4.
+    exact H4.
+    intros.
+    inversion H5; subst.
+    + now apply ChunkedRelatedHead, H0.
     + now apply ChunkedRelatedTail.
 Qed.
 
 Lemma pre_bisimulation_skip
   {a1 a2: p4automaton}
+  (close: rel a1 a2 -> rel a1 a2)
   (checked: chunked_relation a1 a2)
   (front: chunked_relation a1 a2)
   (R: configuration a1 -> configuration a2 -> Prop)
 :
+  closure_mono close ->
   (forall c1 c2, R c1 c2 -> chunked_related checked c1 c2) ->
-  pre_bisimulation checked front ->
-  pre_bisimulation checked (R :: front)
+  pre_bisimulation close checked front ->
+  pre_bisimulation close checked (R :: front)
 .
 Proof.
-  do 7 intro.
-  apply H0; auto.
+  do 8 intro.
+  apply H1; auto.
   - intros c1' c2' ? ?.
-    apply (H2 _ _ b) in H4.
-    rewrite <- app_comm_cons in H4.
-    inversion H4; subst; auto.
-    apply H in H7.
+    apply (H3 _ _ b) in H5.
+    rewrite <- app_comm_cons in H5.
+    eapply H in H5.
+    exact H5.
+    intros.
+    inversion H6; subst; auto.
     apply chunked_related_subset with (R1 := checked); auto.
     solve_incl.
-  - rewrite <- app_comm_cons in H3.
-    inversion H3; subst; auto.
-    apply H in H6.
+  - rewrite <- app_comm_cons in H4.
+    eapply H in H4.
+    exact H4.
+    intros.
+    inversion H5; subst; auto.
     apply chunked_related_subset with (R1 := checked); auto.
     solve_incl.
 Qed.
+
+(* Sanity check: the identity function is a valid closure. *)
+Definition close_id
+  {a1 a2: p4automaton}
+  (R: rel a1 a2)
+:=
+  R
+.
+
+Lemma close_id_preserves_accept
+  {a1 a2: p4automaton}
+:
+  @closure_preserves_accept a1 a2 close_id
+.
+Proof.
+  firstorder.
+Qed.
+
+Lemma close_id_preserves_transition
+  {a1 a2: p4automaton}
+:
+  @closure_preserves_transition a1 a2 close_id
+.
+Proof.
+  firstorder.
+Qed.
+
+Lemma close_id_mono
+  {a1 a2: p4automaton}
+:
+  @closure_mono a1 a2 close_id
+.
+Proof.
+  firstorder.
+Qed.
+
+Lemma close_id_extends
+  {a1 a2: p4automaton}
+:
+  @closure_extends a1 a2 close_id
+.
+Proof.
+  firstorder.
+Qed.
+
+(* This is NOT a valid closure; it does not preserve transitions. *)
+Inductive close_buffer
+  {a1 a2: p4automaton}
+  (R: configuration a1 -> configuration a2 -> Prop)
+  : configuration a1 -> configuration a2 -> Prop
+:=
+| CloseBase:
+    forall c1 c2,
+      R c1 c2 -> close_buffer _ c1 c2
+| CloseStep:
+    forall s1 st1 buf1 s2 st2 buf2 b,
+      close_buffer _ (inl s1, st1, buf1)
+              (inl s2, st2, buf2) ->
+      length buf1 + 1 < size a1 s1 ->
+      length buf2 + 1 < size a2 s2 ->
+      close_buffer _ (inl s1, st1, buf1 ++ b :: nil)
+                     (inl s2, st2, buf2 ++ b :: nil)
+.

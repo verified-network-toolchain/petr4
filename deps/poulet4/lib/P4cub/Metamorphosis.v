2a2abc707671fb4eaa9059c43732b2b584f09e46
John Sarracino <jsarracino@cornell.edu>
2021-06-01 19:34:17 -0700

    add more parser metamorphosis

--- deps/poulet4/lib/P4cub/Metamorphosis.v
+++ deps/poulet4/lib/P4cub/Metamorphosis.v
@@ -13,6 +13,8 @@ Import P4cub.P4cubNotations.
 Require Import Coq.Strings.String.
 Require Import Coq.Classes.EquivDec.
 
+Require Import Poulet4.Monads.Error.
+
 (** * P4light -> P4cub *)
 
 (* General Unresolved Challenges:
@@ -31,6 +33,14 @@ Module ST := P.Stmt.
 Section Metamorphosis.
   Context {tags_t : Type}.
 
+  (* Error type for fine-grained translation errors *)
+  Inductive MorphError :=
+    | Inconceivable
+    | UnsupportedTy (ty: @P4Type tags_t)
+    | UnsupportedExpr (e: @Expression tags_t)
+    | UnsupportedStmt (s: @Statement tags_t)
+    | UnsupportedBop (o: OpBin).
+
   (** Type Metamorphosis.
       QUESTIONS:
       - Why do controls & functions have both
@@ -45,57 +55,57 @@ Section Metamorphosis.
       - Should &, if so, how will
         header unions, strings, enums, varbits, & sets be compiled?
       - Why is there both a record and a struct type? *)
-  Fixpoint type_morph (t : P4Type) : option E.t :=
-    let fix lrec (ts : list P4Type) : option (list E.t) :=
+  Fixpoint type_morph (t : P4Type) : @error_monad MorphError E.t :=
+    let fix lrec (ts : list P4Type) : @error_monad MorphError (list E.t) :=
         match ts with
-        | []     => Some []
+        | []     => mret []
         | t :: ts => t <- type_morph t ;;
                    ts <- lrec ts ;;
-                   Some (t :: ts)
+                   mret (t :: ts)
         end in
-    let fix frec (fs : P4String.AList tags_t P4Type) : option (F.fs string E.t) :=
+    let fix frec (fs : P4String.AList tags_t P4Type) : @error_monad MorphError (F.fs string E.t) :=
         match fs with
-        | []         => Some []
+        | []         => mret []
         | (x,t) :: fs => t <- type_morph t ;;
                        fs <- frec fs ;;
-                       Some ((P4String.str x, t) :: fs)
+                       mret ((P4String.str x, t) :: fs)
         end in
     match t with
-    | TypBool => Some {{ Bool }}
-    | TypInteger => None (* TODO *)
-    | TypString => None (* TODO *)
-    | TypInt w => Some $ E.TInt $ Pos.of_nat w
-    | TypBit w => Some $ E.TBit $ Pos.of_nat w
-    | TypVarBit _ => None (* TODO *)
+    | TypBool => mret {{ Bool }}
+    | TypInteger => err (UnsupportedTy t) (* TODO *)
+    | TypString => err (UnsupportedTy t) (* TODO *)
+    | TypInt w => mret $ E.TInt $ Pos.of_nat w
+    | TypBit w => mret $ E.TBit $ Pos.of_nat w
+    | TypVarBit _ => err (UnsupportedTy t) (* TODO *)
     | TypArray t n =>
       let n := Pos.of_nat n in
-      t <- type_morph t ;;
-      match t with
-      | {{ hdr { ts } }} => Some {{ stack ts[n] }}
-      | _ => None
+      t' <- type_morph t ;;
+      match t' with
+      | {{ hdr { ts } }} => mret {{ stack ts[n] }}
+      | _ => err (UnsupportedTy t)
       end
     | TypTuple ts
     | TypList  ts => ts <<| lrec ts ;; {{ tuple ts }}
     | TypRecord fs
     | TypStruct fs => fs <<| frec fs ;; {{ rec { fs } }}
-    | TypSet _ => None (* TODO *)
-    | TypError => Some {{ error }}
-    | TypMatchKind => Some {{ matchkind }}
-    | TypVoid => None (* TODO *)
+    | TypSet _ => err (UnsupportedTy t) (* TODO *)
+    | TypError => mret {{ error }}
+    | TypMatchKind => mret {{ matchkind }}
+    | TypVoid => err (UnsupportedTy t) (* TODO *)
     | TypHeader fs => fs <<| frec fs ;; {{ hdr { fs } }}
-    | TypHeaderUnion _ => None (* TODO *)
-    | TypEnum _ _ _ => None (* TODO *)
-    | TypTypeName _ => None (* TODO *)
+    | TypHeaderUnion _ => err (UnsupportedTy t) (* TODO *)
+    | TypEnum _ _ _ => err (UnsupportedTy t) (* TODO *)
+    | TypTypeName _ => err (UnsupportedTy t) (* TODO *)
     | TypNewType _ t => type_morph t
-    | TypControl _ => None (* TODO *)
-    | TypParser _  => None (* TODO *)
-    | TypExtern _  => None (* TODO *)
-    | TypFunction _ => None (* TODO *)
-    | TypAction _ _ => None (* TODO *)
-    | TypTable _ => None (* TODO *)
-    | TypPackage _ _ _ => None (* TODO *)
-    | TypSpecializedType _ _ => None (* TODO, type subsitution? *)
-    | TypConstructor _ _ _ _ => None (* TODO, type subsitution? *)
+    | TypControl _ => err (UnsupportedTy t) (* TODO *)
+    | TypParser _  => err (UnsupportedTy t) (* TODO *)
+    | TypExtern _  => err (UnsupportedTy t) (* TODO *)
+    | TypFunction _ => err (UnsupportedTy t) (* TODO *)
+    | TypAction _ _ => err (UnsupportedTy t) (* TODO *)
+    | TypTable _ => err (UnsupportedTy t) (* TODO *)
+    | TypPackage _ _ _ => err (UnsupportedTy t) (* TODO *)
+    | TypSpecializedType _ _ => err (UnsupportedTy t) (* TODO, type subsitution? *)
+    | TypConstructor _ _ _ _ => err (UnsupportedTy t) (* TODO, type subsitution? *)
     end.
   (**[]*)
 
@@ -109,29 +119,29 @@ Section Metamorphosis.
   (**[]*)
 
   (** Binary Operations. *)
-  Definition bop_morph (op : OpBin) : option E.bop :=
+  Definition bop_morph (op : OpBin) : @error_monad MorphError E.bop :=
     match op with
-    | Plus => Some E.Plus
-    | PlusSat => Some E.PlusSat
-    | Minus => Some E.Minus
-    | MinusSat => Some E.MinusSat
-    | Mul => Some E.Times
-    | Div => None
-    | Mod => None
-    | Shl => Some E.Shl
-    | Shr => Some E.Shr
-    | Le => Some E.Le
-    | Ge => Some E.Ge
-    | Lt => Some E.Lt
-    | Gt => Some E.Gt
-    | Eq => Some E.Eq
-    | NotEq => Some E.NotEq
-    | BitAnd => Some E.BitAnd
-    | BitXor => Some E.BitXor
-    | BitOr => Some E.BitOr
-    | PlusPlus => Some E.PlusPlus
-    | And => Some E.And
-    | Or => Some E.Or
+    | Plus => mret E.Plus
+    | PlusSat => mret E.PlusSat
+    | Minus => mret E.Minus
+    | MinusSat => mret E.MinusSat
+    | Mul => mret E.Times
+    | Div => err (UnsupportedBop op)
+    | Mod => err (UnsupportedBop op)
+    | Shl => mret E.Shl
+    | Shr => mret E.Shr
+    | Le => mret E.Le
+    | Ge => mret E.Ge
+    | Lt => mret E.Lt
+    | Gt => mret E.Gt
+    | Eq => mret E.Eq
+    | NotEq => mret E.NotEq
+    | BitAnd => mret E.BitAnd
+    | BitXor => mret E.BitXor
+    | BitOr => mret E.BitOr
+    | PlusPlus => mret E.PlusPlus
+    | And => mret E.And
+    | Or => mret E.Or
     end.
   (**[]*)
 
@@ -160,16 +170,16 @@ Section Metamorphosis.
       5. Why is [N] still used in [Syntax.v]?
          Didn't we decide it was undesirable to
          use both [N] and [Z]? *)
-  Fixpoint expr_morph (e : Expression) : option (E.e tags_t) :=
-    let fix lrec (es : list Expression) : option (list (E.e tags_t)) :=
+  Fixpoint expr_morph (e : Expression) : @error_monad MorphError (E.e tags_t) :=
+    let fix lrec (es : list Expression) : @error_monad MorphError (list (E.e tags_t)) :=
         match es with
-        | []     => Some []
+        | []     => mret []
         | e :: es => e <- expr_morph e ;;
                    es <<| lrec es ;; e :: es
         end in
-    let fix frec (fs : list KeyValue) : option (F.fs string (E.t * E.e tags_t)) :=
+    let fix frec (fs : list KeyValue) : @error_monad MorphError (F.fs string (E.t * E.e tags_t)) :=
         match fs with
-        | [] => Some []
+        | [] => mret []
         | (MkKeyValue _ x ((MkExpression _ _ t _) as e))
             :: fs => t <- type_morph t ;;
                    e <- expr_morph e ;;
@@ -177,23 +187,23 @@ Section Metamorphosis.
                    (P4String.str x, (t, e)) :: fs
         end in
     match e with
-    | MkExpression i (ExpBool b) _ _ => Some <{ BOOL b @ i }>
+    | MkExpression i (ExpBool b) _ _ => mret <{ BOOL b @ i }>
     | MkExpression i (ExpInt n)  _ _
       => let z := P4Int.value n in
-        wsigned <- P4Int.width_signed n ;;
-        match wsigned with
-        | (w, false) => let w := Pos.of_nat w in Some <{ w W z @ i }>
-        | (w, true)  => let w := Pos.of_nat w in Some <{ w S z @ i }>
+        match P4Int.width_signed n with
+        | Some (w, false) => let w := Pos.of_nat w in mret <{ w W z @ i }>
+        | Some (w, true)  => let w := Pos.of_nat w in mret <{ w S z @ i }>
+        | None => err (UnsupportedExpr e)
         end
     | MkExpression i (ExpName (BareName x) _) t _
       => t <<| type_morph t ;;
         E.EVar t (P4String.str x) i
     | MkExpression i (ExpArrayAccess e1 e2) _ _
       => e1 <- expr_morph e1 ;;
-        e2 <- expr_morph e2 ;;
-        match e2 with
-        | <{ _ W idx @ _ }> => Some <{ Access e1[idx] @ i }>
-        | _ => None (* TODO *)
+        e2' <- expr_morph e2 ;;
+        match e2' with
+        | <{ _ W idx @ _ }> => mret <{ Access e1[idx] @ i }>
+        | _ => err $ UnsupportedExpr e2 (* TODO *)
         end
     | MkExpression i (ExpBitStringAccess e lo hi) t _
       => let lo := Pos.pred $ N.succ_pos lo in
@@ -220,18 +230,18 @@ Section Metamorphosis.
       => t <- type_morph t ;;
         e <<| expr_morph e ;; <{ Cast e:t @ i }>
     | MkExpression i (ExpErrorMember err) _ _
-      => Some $ E.EError (Some $ P4String.str err) i
+      => mret $ E.EError (mret $ P4String.str err) i
     | MkExpression i (ExpExpressionMember e f) t _ 
       => 
         let f := P4String.str f in 
         t <- type_morph t ;;
         e <<| expr_morph e ;; 
         <{ Mem e:t dot f @ i }>
-    | _ => None
+    | _ => err (UnsupportedExpr e)
     end.
   (**[]*)
 
-  Definition type_expr_morph (e : Expression) : option (E.t * E.e tags_t) :=
+  Definition type_expr_morph (e : Expression) : @error_monad MorphError (E.t * E.e tags_t) :=
     match e with
     | MkExpression i _ t _ => t <- type_morph t ;;
                              e <<| expr_morph e ;; (t, e)
@@ -251,10 +261,10 @@ Section Metamorphosis.
          - method calls? Type subsitution?
       3. How to get parameter names to generate arguments?
       4. When and how will instantiations be lifted? *)
-  Fixpoint stmt_morph (s : Statement) : option (ST.s tags_t) :=
-    let fix blk_morph (blk : Block) : option (ST.s tags_t) :=
+  Fixpoint stmt_morph (s : Statement) : @error_monad MorphError (ST.s tags_t) :=
+    let fix blk_morph (blk : Block) : @error_monad MorphError (ST.s tags_t) :=
         match blk with
-        | BlockEmpty i => Some -{ skip @ i }-
+        | BlockEmpty i => mret -{ skip @ i }-
         | BlockCons ((MkStatement i _ _) as s) blk
           => s1 <- stmt_morph s ;;
             s2 <<| blk_morph blk ;; -{ s1; s2 @ i }-
@@ -263,9 +273,9 @@ Section Metamorphosis.
         match swcase with
         end in *)
     match s with
-    | MkStatement i StatEmpty _ => Some -{ skip @ i }-
-    | MkStatement i StatExit  _ => Some -{ exit @ i }-
-    | MkStatement i (StatReturn None) _ => Some -{ returns @ i }-
+    | MkStatement i StatEmpty _ => mret -{ skip @ i }-
+    | MkStatement i StatExit  _ => mret -{ exit @ i }-
+    | MkStatement i (StatReturn None) _ => mret -{ returns @ i }-
     | MkStatement
         i (StatReturn (Some ((MkExpression _ _ t _) as e))) _
       => t <- type_morph t ;;
@@ -301,7 +311,7 @@ Section Metamorphosis.
                   (MkExpression _ 
                     (ExpName (BareName x) _) 
                     (TypTypeName (BareName xty)) 
-                  _)
+                    _)
                   fname)
                 (TypFunction (MkFunctionType _ _ FunExtern retty))
                 _)
@@ -318,16 +328,105 @@ Section Metamorphosis.
             match args with 
             | Some e :: nil => 
               let* '(t, e') := type_expr_morph e in
-              Some (("arg", P4cub.PAOut (t, e')) :: nil)
-            | _ => Some nil
+              mret (("arg", P4cub.PAOut (t, e')) :: nil)
+            | _ => mret nil
             end ;;
-          ty' <- Some None ;;
-          Some -{ extern x calls fname with args' gives ty' @ i }-
-        else None
+          ty' <- mret None ;;
+          mret -{ extern x calls fname with args' gives ty' @ i }-
+        else err (UnsupportedStmt s)
     | MkStatement i (StatSwitch e cases) _
-      => e <- expr_morph e ;; None (* TODO *)
-    | MkStatement i _ _ => Some -{ exit @ i }-
-    (* | _ => None *)
+      => e <- expr_morph e ;; err (UnsupportedStmt s) (* TODO *)
+    | MkStatement _ _ _ => err (UnsupportedStmt s)
     end.
   (**[]*)
+
+  Definition parser_case_morph 
+    (c: @ParserCase tags_t) 
+    : @error_monad MorphError (P.Parser.pat * string) :=
+    let 'MkParserCase tag matches next := c in 
+    let combine_pats := fun (p: list P.Parser.pat) =>
+      match p with 
+      | pat :: nil => mret pat
+      | _ :: _ => mret [{ PTUP p }]
+      | nil => err Inconceivable
+      end in
+    let match_worker (m: Match) := 
+      let 'MkMatch tag m' ty := m in 
+      match m' with 
+      | MatchDontCare => mret [{ ?? }]
+      | MatchExpression me => 
+        e' <- expr_morph me ;; 
+        match e' with 
+        | <{ w W n @ _ }> => mret [{ w PW n }]
+        | <{ w S n @ _ }> => mret [{ w PS n }]
+        | _ => err Inconceivable
+        end
+      end in 
+    pats <- sequence (map match_worker matches) ;;
+    pats' <- combine_pats pats ;;
+    mret (pats', P4String.str next).
+
+
+  Definition morph_str (n: string) := 
+    match n with 
+    | "start" => ={ start }=
+    | "accept" => ={ accept }=
+    | "reject" => ={ reject }=
+    | name => ={ δ name }=
+    end.
+
+    Definition morph_name (n: P4String.t tags_t) := morph_str (P4String.str n).
+
+  Fixpoint get_default 
+    (tag: tags_t) 
+    (cases: list (P.Parser.pat * string)) 
+    : @error_monad MorphError (P.Parser.e tags_t) :=
+    match cases with 
+    | nil => err Inconceivable
+    | ([{ ?? }], s) :: _ => 
+      let st := morph_str s in 
+      mret p{ goto st @ tag }p
+    | _ :: cases => get_default tag cases
+    end.
+
+  Definition morph_cexpr 
+    (tag: tags_t)
+    (x: P.Parser.pat * string) 
+    : P.Parser.pat * (P.Parser.e tags_t) := 
+    let '(pat, nxt) := x in 
+    let st := morph_str nxt in 
+    let nxt_jump := p{ goto st @ tag }p in 
+    (pat, nxt_jump). 
+
+  Definition parser_trans_morph
+    (t: @ParserTransition tags_t)
+    : @error_monad MorphError (P.Parser.e tags_t) :=
+    match t with 
+    | ParserDirect tag next_name => 
+      let st := morph_name next_name in 
+      mret p{ goto st @ tag }p
+    | ParserSelect tag es cases =>
+      cub_cases <- sequence (map parser_case_morph cases) ;; 
+      cub_exprs <- sequence (map expr_morph es) ;;
+      def <- get_default tag cub_cases ;;
+      let exp := E.ETuple cub_exprs tag in 
+      let cub_cases' := map (fun '(k, v) => let v' := morph_str v in (k, p{ goto v' @ tag }p)) cub_cases in 
+      mret p{ select exp { cub_cases' } default := def @ tag }p
+    end.
+
+  Fixpoint combine_ss (tag: tags_t) (ss: list (ST.s tags_t)) : ST.s tags_t := 
+    match ss with 
+    | nil => -{ skip @ tag }-
+    | s :: ss' => 
+      ST.SSeq s (combine_ss tag ss') tag
+    end.
+
+  Definition parser_state_morph 
+    (ps: @ParserState tags_t) 
+    : @error_monad MorphError (P.Parser.state_block tags_t) :=
+    let 'MkParserState tag name ss trans := ps in 
+    ss' <- sequence (map stmt_morph ss) ;; 
+    trans' <- parser_trans_morph trans ;; 
+    mret (P.Parser.State (combine_ss tag ss') trans').
+
 End Metamorphosis.

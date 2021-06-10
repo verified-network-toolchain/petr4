34890d3526f91265771df09f4e87157b98f949ff
Ryan Doenges <me@ryandoeng.es>
2021-06-09 21:02:06 -0400

    define semantics for automata syntax

--- deps/poulet4/lib/P4automata/Syntax.v
+++ deps/poulet4/lib/P4automata/Syntax.v
@@ -1,3 +1,4 @@
+Require Coq.Lists.List.
 Require Import String.
 Require Import HAList.
 Require Poulet4.P4cub.Envn.
@@ -6,11 +7,12 @@ Require Poulet4.P4automata.P4automaton.
 Module P4A := P4automaton.
 
 Module Env := Poulet4.P4cub.Envn.Env.
-Module EnvUtil := Poulet4.P4cub.BigStep.BSUtil.EnvUtil.
+
+Open Scope list_scope.
 
 Inductive hdr_ref: Type :=
-| HRVar (var: string)
-| HRField (hdr: hdr_ref) (field: string).
+| HRVar (var: string).
+(*| HRField (hdr: hdr_ref) (field: string).*)
 
 Inductive expr :=
 | EHdr (h: hdr_ref)
@@ -23,23 +25,33 @@ Inductive state_name: Type :=
 
 Definition state_ref: Type := state_name + bool.
     
+Inductive v :=
+| VBits : list bool -> v.
+
+Definition v_eq_dec (v1 v2: v) : {v1 = v2} + {v1 <> v2}.
+Proof.
+  destruct v1, v2.
+  destruct (List.list_eq_dec Bool.bool_dec l l0).
+  - left; congruence.
+  - right; congruence.
+Defined.
+
 Record sel_case: Type :=
-  { sc_val: list bool;
+  { sc_val: v;
     sc_st: state_ref }.
 
 Inductive transition: Type :=
 | TGoto (state: state_ref)
-| TSel (cond: hdr_ref) (cases: list sel_case) (default: state_ref).
+| TSel (cond: expr) (cases: list sel_case) (default: state_ref).
 
 Inductive op :=
 | OpNil
-| OpSeq (s1 s2: op)
-| OpExtract (width: BinNums.positive) (hdr: hdr_ref)
-| OpAsgn (lhs: hdr_ref) (rhs: expr)
-| OpBlock (o: op).
+| OpSeq (o1 o2: op)
+| OpExtract (width: nat) (hdr: hdr_ref)
+| OpAsgn (lhs: hdr_ref) (rhs: expr).
 
 Record state: Type :=
-  { st_hdr: hdr_ref;
+  { st_op: op;
     st_trans: transition }.
 
 Definition t: Type :=
@@ -51,21 +63,99 @@ Section Interp.
   Definition state_type :=
     { s: state_name | a s <> None }.
   
-  Definition store := EnvUtil.epsilon.
+  Definition store := Env.t string v.
   
-  Definition pre_size (state: state_name) : nat.
-  Admitted.
+  Fixpoint op_size (o: op) : nat :=
+    match o with
+    | OpNil => 0
+    | OpSeq o1 o2 =>
+      op_size o1 + op_size o2
+    | OpExtract width _ => width
+    | OpAsgn _ _ => 0
+    end.
 
-  Definition size (state: state_type) : nat.
-  Admitted.
+  Definition find_state (st: state_type) : state.
+  Proof.
+    destruct (a (proj1_sig st)) eqn:?.
+    - exact s.
+    - exfalso.
+      apply (proj2_sig st).
+      apply Heqo.
+  Defined.
+
+  Definition size (state: state_type) : nat :=
+    op_size (find_state state).(st_op).
+
 
   Variable (has_extract: forall s H, 0 < size (exist _ s H)).
 
-  Definition update (state: state_type) (bits: list bool) (st: store) : store.
-  Admitted.
+  Definition assign (h: hdr_ref) (v: v) (s: store) : store :=
+    match h with
+    | HRVar x => Env.bind x v s
+    end.
+
+  Search (nat -> list ?A -> list ?A).
+
+  Definition eval_expr (st: store) (e: expr) : v :=
+   match e with
+   | EHdr (HRVar x) =>
+     match st x with
+     | Some v => v
+     | None => VBits nil
+     end
+   | ELit bs => VBits bs
+   end.
+
+  Fixpoint eval_op (st: store) (bits: list bool) (o: op) : store :=
+    match o with
+    | OpNil => st
+    | OpSeq o1 o2 =>
+      eval_op (eval_op st bits o1) bits o2
+    | OpExtract width hdr =>
+      assign hdr (VBits (List.firstn width bits)) st
+    | OpAsgn hdr expr =>
+      assign hdr (eval_expr st expr) st
+    end.
+
+  Definition update (state: state_type) (bits: list bool) (st: store) : store :=
+    eval_op st bits (find_state state).(st_op).
   
-  Definition transitions (s: state_type) (st: store) : state_type + bool.
-  Admitted.
+  Fixpoint pre_eval_sel (st: store) (cond: v) (cases: list sel_case) (default: state_ref) : state_ref :=
+    match cases with
+    | c::cases =>
+      if v_eq_dec cond c.(sc_val)
+      then c.(sc_st)
+      else pre_eval_sel st cond cases default
+    | nil => default
+    end.
+
+  Definition clamp_state_name (s: state_name) : state_type + bool.
+  Proof.
+    destruct (a s) eqn:?.
+    - left.
+      exists s.
+      congruence.
+    - exact (inr false).
+  Defined.
+
+  Definition clamp_state_ref (s: state_ref) : state_type + bool :=
+    match s with
+    | inl s => clamp_state_name s
+    | inr b => inr b
+    end.
+  
+  Definition eval_sel (st: store) (cond: v) (cases: list sel_case) (default: state_ref) : state_type + bool :=
+    clamp_state_ref (pre_eval_sel st cond cases default).
+
+  Definition eval_trans (st: store) (t: transition) : state_type + bool :=
+    match t with
+    | TGoto state => clamp_state_ref state
+    | TSel cond cases default =>
+      eval_sel st (eval_expr st cond) cases default
+    end.
+
+  Definition transitions (s: state_type) (st: store) : state_type + bool :=
+    eval_trans st (find_state s).(st_trans).
 
   Lemma cap: forall s, 0 < size s.
   Proof.
@@ -82,5 +172,3 @@ Section Interp.
        P4A.transitions := transitions;
        P4A.cap := cap |}.
 End Interp.
-
-                    

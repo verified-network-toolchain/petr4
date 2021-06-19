9b6acbec51b4f53092a4a9030778c1fddc0b862d
Shengyi Wang <txyyss@gmail.com>
2021-06-20 01:35:55 +0800

    Proved two axioms in AList.v.

--- deps/poulet4/lib/AList.v
+++ deps/poulet4/lib/AList.v
@@ -44,16 +44,39 @@ Section AList.
       if KEqDec k k'
       then ((k, v) :: l')
       else ((k', v') :: (set_some l' k v))
-    | nil => nil
+    | nil =>[(k, v)]
     end.
 
-  Axiom get_some_set: forall l k v1 v2,
+  Lemma get_neq_cons:
+    forall (k' : K) (v' : V) (l : list (K * V)) (k : K),
+      k =/= k' -> get ((k', v') :: l) k = get l k.
+  Proof. intros. unfold get. simpl. destruct (KEqDec k k'); easy. Qed.
+
+  Lemma get_some_set: forall l k v1 v2,
                       get l k = Some v1 ->
                       set l k v2 = Some (set_some l k v2).
+  Proof.
+    induction l; intros.
+    - unfold get in H0. simpl in H0. inversion H0.
+    - simpl. destruct a as [k' v']. destruct (KEqDec k k'); auto.
+      assert (get l k = Some v1) by now rewrite get_neq_cons in H0.
+      destruct (set l k v2) eqn: ?.
+      + rewrite (IHl _ v1) in Heqo; auto. inversion Heqo. easy.
+      + exfalso. clear -H1 Heqo. rename Heqo into H2. induction l.
+        * unfold get in H1. simpl in H1. inversion H1.
+        * simpl in H2. destruct a as [k' v']. destruct (KEqDec k k'). 1: inversion H2.
+          destruct (set l k v2). 1: inversion H2. apply IHl; auto.
+          now rewrite get_neq_cons in H1.
+  Qed.
 
-  Axiom set_some_get: forall l k v,
-                      get (set_some l k v) k = Some v.
-
+  Lemma set_some_get: forall l k v, get (set_some l k v) k = Some v.
+  Proof.
+    induction l; intros.
+    - simpl. unfold get. simpl. destruct (KEqDec k k); auto. exfalso. now apply c.
+    - destruct a as [k' v']. simpl. destruct (KEqDec k k').
+      + unfold get. simpl. destruct (KEqDec k k); auto. exfalso. now apply c.
+      + rewrite get_neq_cons; auto.
+  Qed.
 
   Fixpoint key_unique (l : AList K V R) : bool :=
     match l with

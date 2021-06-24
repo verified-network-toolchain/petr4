bc4be22e5d9775ed2dbedf8182369f3d5882a85e
John Chen <jc2786@cornell.edu>
2021-06-24 14:03:11 -0400

    move ccompiler codes into its own folder and change signature to expr-> (list stmt) * expr

--- deps/poulet4/lib/Ccompiler/CComp.v
+++ deps/poulet4/lib/Ccompiler/CComp.v
@@ -5,7 +5,7 @@ Require Import Coq.PArith.BinPos.
 Require Import Poulet4.P4cub.Envn.
 Require Import Poulet4.Monads.Monad.
 Require Import Poulet4.Monads.Option.
-
+Require Import List.
 Module P := P4cub.
 Module F := P.F.
 Module E := P.Expr.
@@ -28,64 +28,67 @@ Section CComp.
   Definition identMap : Type := Env.t string AST.ident.
   Definition CTranslateType (p4t : E.t) : Ctypes.type.
     Admitted.
-  Definition CTranslateTypeRev (ct : Ctypes.type) : E.t.
+  Definition CubTypeOf (e : E.e tags_t) : E.t.
     Admitted.
-  Definition CubTypeOf (ce : Clight.expr) : E.t := 
-    CTranslateTypeRev (Clight.typeof ce).
-  Definition CTranslateSlice (x: Clight.expr) (hi lo: positive) (type: E.t) : option Clight.expr := 
+  Definition CTranslateSlice (x: Clight.expr) (hi lo: positive) (type: E.t) : option ( (list Clight.statement) * Clight.expr) := 
     (* x[hi : lo] = [x >> lo] & (1<<(hi-lo+1) - 1)*)
     let hi' := Econst_int (Integers.Int.repr (Zpos hi)) (int_unsigned) in
     let lo' := Econst_int (Integers.Int.repr (Zpos lo)) (int_unsigned) in
     let one' := Econst_long (Integers.Int64.one) (long_unsigned) in
-    Some (Ebinop Oand (Ebinop Oshr x lo' long_unsigned) 
+    Some ([], Ebinop Oand (Ebinop Oshr x lo' long_unsigned) 
           (Ebinop Osub (Ebinop Oshl one' (Ebinop Oadd one' 
           (Ebinop Osub hi' lo' long_unsigned) long_unsigned) long_unsigned) 
             (one') long_unsigned) long_unsigned).
-  Definition CTranslateCast (e: Clight.expr) (typefrom typeto: E.t) : option Clight.expr.
+  Definition CTranslateCast (e: Clight.expr) (typefrom typeto: E.t) : option ( (list Clight.statement) * Clight.expr).
   Admitted.
 
-  Definition CTranslateUop (op: E.uop) : option Cop.unary_operation := 
-    match op with
-    | _{!}_ => Some Onotbool
-    | _{~}_ => Some Onotint
-    | _{-}_ => Some Oneg
-    | _ => None
-    end.
+  Definition CTranslateUop (op: E.uop) (x) :  option ( (list Clight.statement) * Clight.expr).
+  Admitted.
 
-  Definition CTranslateBop (op: E.bop) : Cop.binary_operation
-    match op with 
-    | _ => None
-    end. 
+  Definition CTranslateBop (op: E.bop) : Cop.binary_operation.
+  Admitted. 
   (* TODO: figure out what cast rules does clight support and then implement this*)
   (* See https://opennetworking.org/wp-content/uploads/2020/10/P416-Language-Specification-wd.html#sec-casts *)
-  Fixpoint CTranslateExpr (e: E.e tags_t) (idents: identMap): option Clight.expr :=
+  Fixpoint CTranslateExpr (e: E.e tags_t) (idents: identMap): option ( (list Clight.statement) * Clight.expr) :=
+    let prepend (addon: list Clight.statement) (source: option ( (list Clight.statement) * Clight.expr)): option ( (list Clight.statement) * Clight.expr) := 
+      match source with
+      | Some (src_list, e) => Some (addon++src_list, e)
+      | None => None
+      end
+    in
+    let postpend (addon: list Clight.statement) (source: option ( (list Clight.statement) * Clight.expr)): option ( (list Clight.statement) * Clight.expr) := 
+      match source with
+      | Some (src_list, e) => Some (src_list++addon, e)
+      | None => None
+      end
+    in
     match e with
-    | <{TRUE @ i}> =>   Some (Econst_int (Integers.Int.one) (type_bool))
-    | <{FALSE @ i}> =>  Some (Econst_int (Integers.Int.zero) (type_bool))
+    | <{TRUE @ i}> =>   Some ([], Econst_int (Integers.Int.one) (type_bool))
+    | <{FALSE @ i}> =>  Some ([], Econst_int (Integers.Int.zero) (type_bool))
     
     | <{w W n @ i}> =>  if (Pos.leb w (Pos.of_nat 64))
-                        then Some (Econst_long (Integers.Int64.repr n) (long_unsigned))
+                        then Some ([], Econst_long (Integers.Int64.repr n) (long_unsigned))
                         else None 
     | <{w S n @ i}> =>  if (Pos.leb w (Pos.of_nat 64))
-                        then Some(Econst_long (Integers.Int64.repr n) (long_signed))
+                        then Some([], Econst_long (Integers.Int64.repr n) (long_signed))
                         else None
     | <{Var x : ty @ i}> => (*assuming p4cub separates initialization and declaration*)
                         match Env.find x idents with
-                        | Some id => Some (Evar id (CTranslateType ty))
+                        | Some id => Some ([], Evar id (CTranslateType ty))
                         | _ => None
                         end 
     | <{Slice n : τ [ hi : lo ] @ i}> => 
                         match CTranslateExpr n idents with
-                        | Some n' => CTranslateSlice n' hi lo τ
+                        | Some (stmts, n') => prepend stmts (CTranslateSlice n' hi lo τ)
                         | _ => None
                         end
     | <{Cast e : τ @ i}> => 
                         match CTranslateExpr e idents with
-                        | Some e' => let typefrom = CubTypeOf e' in 
-                                      CTranslateCast e typefrom τ.
+                        | Some e' => let typefrom := (CubTypeOf e) in 
+                                      CTranslateCast (snd e') typefrom τ
                         | _ => None
                         end
-    (* | <{UOP op x : ty @ i}> =>  *)
+    | <{UOP op x : ty @ i}> => 
     (* | BOP x : tx op y : ty @ i => *)
     (* | tup es @ i => *)
     (* | rec { fields } @ i =>  *)

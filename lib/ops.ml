99753b36233e666cd52b0f62f4870afc59ef6507
Ryan Doenges <me@ryandoeng.es>
2021-07-14 18:26:17 -0400

    don't implement equality for VNull

--- lib/ops.ml
+++ lib/ops.ml
@@ -213,9 +213,6 @@ let rec interp_beq (l : V.value) (r : V.value) : V.value =
   | VUnion{fields=l1},
     VUnion{fields=l2}                         -> unions_equal l1 l2
   | VTuple l1, VTuple l2                      -> tuples_equal l1 l2
-  | VNull, VNull -> VBool true
-  | VNull, _
-  | _, VNull -> VBool false
   | _ -> raise_s [%message "equality comparison undefined for given types"
                      ~l:(l:V.value) ~r:(r:V.value)]
 

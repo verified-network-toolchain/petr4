f85db8b31b76e0564953b8c8a5c294a713f4dca1
John Chen <jc2786@cornell.edu>
2021-07-30 12:56:28 -0400

    Revert "Revert "remove compcert from poulet4_Ccomp""
    This reverts commit 83b5bab958ef6a2cbec4145d13f2afb45338d900.

--- deps/poulet4_Ccomp/CompCert/cparser/tests/handwritten/dubious-enum.c
+++
@@ -1,7 +0,0 @@
-int f (void)
-{
-  int x = sizeof(enum e;
-  /* Maybe a closing parenthesis is missing,
-     maybe also "enum e" could be continued with an opening brace.
-     Our message ignores the latter possibility. */
-}

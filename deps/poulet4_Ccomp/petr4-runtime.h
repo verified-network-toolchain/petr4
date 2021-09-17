62e5f01f814a4a72d64672b2ea18523ed7ee3cf4
Natalie Isak <ngi2@cornell.edu>
2021-09-16 23:49:07 -0400

    modifying runtime --> adding add, multiply

--- deps/poulet4_Ccomp/petr4-runtime.h
+++ deps/poulet4_Ccomp/petr4-runtime.h
@@ -1,30 +1,82 @@
-// Need to expose GMP operations for each P4 construct
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <gmp.h>
+#include <assert.h>
+#include <math.h> 
 
-typedef void *$1;//packet in
-typedef void *$2;//packet out
-typedef void *$3;//bit vec
+typedef void *$pkt_in;
+typedef void *$pkt_out;
+typedef void *$BitVec;
 
-void extract($1 pkt, void *data, int len);
-void emit($2 pkt, void *data, int len);
-
-struct bit_vec
+struct BitVec
 {
-  //define with gmp
-  //width
-  //sign
+  //True = signed, False = unsigned 
+  bool signed;
+
+  //representation of the binary width of the number representation of the bit_vec
+  int width;
+
+  //the GMP representation of the number. 
+  //0<=value<=2^width-1 
+  mpz_t value; 
 };
 
+void reset_bitvec (mpz_t x) {
+  mpz_clear(x);
+}
+
+enum operation{
+  Plus,
+  PlusSat 
+  Mul
+};
+
+/**
+Functions: includes package processing, unary operations, and binary operations 
+**/
+
+//package processing
+void extract($pkt_in pkt, void *data, int len);
+void emit($pkt_out pkt, void *data, int len);
+
 //unary operators
-//always know the width because you can extract it from the type
-void eval_bitnot($3 v, $3 dst);
-void eval_uminus($3 v, $3 dst);
+void eval_uminus($BitVec v, $BitVec dst) {
+  init_bitvec(v.input, n);
+  mpz_neg(n, v_bitvec);
+  dst = bit_vec(mpz_get_str(NULL, 10, n));
+  reset_bitvec(n); 
+}
 
 //binary operators
-// check that params have the same type -
-void unsigned_plus($3 l, $3 r, $3 dst);
-void signed_plus($3 l, $3 r, $3 dst);
-$3 interp_bplus($3 l, $3 r);
+void eval_sat_add(mpz_t dst_value, const mpz_t l_value, const mpz_t r_value) {
+  switch (l.signed, r.signed):
+    case (True, True):
+      mpz_add(dst_value, l_value, r_value);
+      mpz_mod(dst.value, dst.value, dst.width);
+    default:
+      dst.signed = True; 
+      mpz_add(dst_value, l_value, r_value);
+      mpz_mod(dst.value, dst.value, pow(2, dst.width)-1);
+}
+
+void interp_binary_op(operation op, $BitVec l, $BitVec r) {
+  mpz_t dst_value;
+  mpz_init(dst_value);
+  mpz_set_ui(dst_value, 0);
+  struct BitVec dst = { .signed = False,
+                        .width = l.width, //assumption: width of l and r are =
+                        .value = dst_value }
+
+  switch (op):
+    case Plus:
+      mpz_add(dst.value, l.value, r.value);
+      mpz_mod(dst.value, dst.value, dst.width);
+    case PlusSat:
+      eval_sat_add(dst.value, l.value, r.value);
+    case Mul:
+      mpz_mul(dst.value, l.value, r.value);
+      mpz_mod(dst.value, dst.value, dst.width);
+    default:
+      failwith("unimplemented")
+}

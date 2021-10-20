d5bfa13013ecc0f06add64c75fe4ea2f5d4eeb9f
John Chen <jc2786@cornell.edu>
2021-10-20 00:07:59 -0400

    More changes to statementize

--- deps/poulet4/lib/P4cub/Transformations/Statementize.v
+++ deps/poulet4/lib/P4cub/Transformations/Statementize.v
@@ -100,89 +100,178 @@ Fixpoint TransformExpr (expr : e) (env: VarNameGen.t)
     let assign := P4cub.Stmt.SAssign type (P4cub.Expr.EVar type var_name i) (P4cub.Expr.EUop op type arg' i) i in 
     let stmt := P4cub.Stmt.SSeq arg_stmt (P4cub.Stmt.SSeq declaration assign i) i in
     (stmt, P4cub.Expr.EVar type var_name i, env')
-(* 
+
   | P4cub.Expr.EBop op lhs_type rhs_type lhs rhs i => 
-    let (lhs_result, env_lhs) := TransformExpr lhs env in 
-    let (lhs_stmt, lhs') := lhs_result in
-    let (rhs_result, env_rhs) := TransformExpr rhs env_lhs in 
-    let (rhs_stmt, rhs') := rhs_result in
+    let '(lhs_stmt, lhs', env_lhs) := TransformExpr lhs env in 
+    let '(rhs_stmt, rhs', env_rhs) := TransformExpr rhs env_lhs in 
     let (var_name, env') := VarNameGen.new_var env_rhs in
     let dst_type := lhs_type in (*the result type is always the same as lhs type*)
-    let declaration := SelS.SVardecl dst_type var_name i in
-    let assign := SelS.SBop dst_type op lhs' rhs' var_name i in 
-    let stmt := SelS.SSeq lhs_stmt (SelS.SSeq rhs_stmt (SelS.SSeq declaration assign i) i) i in
-    ( (stmt, SelE.EVar dst_type var_name i), env')
+    let declaration := P4cub.Stmt.SVardecl dst_type var_name i in
+    let assign := P4cub.Stmt.SAssign dst_type (P4cub.Expr.EVar dst_type var_name i) (P4cub.Expr.EBop op lhs_type rhs_type lhs' rhs' i) i in 
+    let stmt := P4cub.Stmt.SSeq lhs_stmt (P4cub.Stmt.SSeq rhs_stmt (P4cub.Stmt.SSeq declaration assign i) i) i in
+    (stmt, P4cub.Expr.EVar dst_type var_name i, env')
 
-  | P4cub.Expr.ETuple es i =>
-    let type := CubE.TBool in 
-    let '((es_stmt, es'), env_es) := TranslateExprList es env i in 
+  | P4cub.Expr.ETuple es i => (*TODO: get the correct type*)
+    let type := P4cub.Expr.TBool in 
+    let '(es_stmt, es', env_es) := TransformExprList es env i in 
     let (var_name, env') := VarNameGen.new_var env_es in
-    let declaration := SelS.SVardecl type var_name i in
-    let assign := SelS.STuple es' var_name i in 
-    let stmt := SelS.SSeq es_stmt (SelS.SSeq declaration assign i) i in
-    ( (stmt, SelE.EVar type var_name i), env')
+    let declaration := P4cub.Stmt.SVardecl type var_name i in
+    let assign := P4cub.Stmt.SAssign type (P4cub.Expr.EVar type var_name i) (P4cub.Expr.ETuple es' i) i in 
+    let stmt := P4cub.Stmt.SSeq es_stmt (P4cub.Stmt.SSeq declaration assign i) i in
+    (stmt, P4cub.Expr.EVar type var_name i, env')
 
-  | P4cub.Expr.EStruct fields i => 
-    let type := CubE.TBool in 
-    let '((fs_stmt, fs'), env_fs) := TranslateFields fields env i in 
+  | P4cub.Expr.EStruct fields i => (*TODO: get the correct type*)
+    let type := P4cub.Expr.TBool in 
+    let '(fs_stmt, fs', env_fs) := TransformFields fields env i in 
     let (var_name, env') := VarNameGen.new_var env_fs in
-    let declaration := SelS.SVardecl type var_name i in
-    let assign := SelS.SStruct fs' var_name i in 
-    let stmt := SelS.SSeq fs_stmt (SelS.SSeq declaration assign i) i in
-    ( (stmt, SelE.EVar type var_name i), env')
+    let declaration := P4cub.Stmt.SVardecl type var_name i in
+    let assign := P4cub.Stmt.SAssign type (P4cub.Expr.EVar type var_name i) (P4cub.Expr.EStruct fs' i) i in 
+    let stmt := P4cub.Stmt.SSeq fs_stmt (P4cub.Stmt.SSeq declaration assign i) i in
+    (stmt, P4cub.Expr.EVar type var_name i, env')
 
-  | P4cub.Expr.EHeader fields valid i =>
-    let type := CubE.TBool in 
-    let '((fs_stmt, fs'), env_fs) := TranslateFields fields env i in 
-    let '((valid_stmt, valid'), env_valid) := TransformExpr valid env in
+  | P4cub.Expr.EHeader fields valid i =>(*TODO: get the correct type*)
+    let type := P4cub.Expr.TBool in 
+    let '(fs_stmt, fs', env_fs) := TransformFields fields env i in 
+    let '(valid_stmt, valid', env_valid) := TransformExpr valid env in
     let (var_name, env') := VarNameGen.new_var env_valid in
-    let declaration := SelS.SVardecl type var_name i in
-    let assign := SelS.SHeader fs' var_name valid' i in 
-    let stmt := SelS.SSeq fs_stmt (SelS.SSeq valid_stmt (SelS.SSeq declaration assign i) i) i in
-    ( (stmt, SelE.EVar type var_name i), env')
+    let declaration := P4cub.Stmt.SVardecl type var_name i in
+    let assign := P4cub.Stmt.SAssign type (P4cub.Expr.EVar type var_name i) (P4cub.Expr.EHeader fs' valid' i) i in 
+    let stmt := P4cub.Stmt.SSeq fs_stmt (P4cub.Stmt.SSeq valid_stmt (P4cub.Stmt.SSeq declaration assign i) i) i in
+    (stmt, P4cub.Expr.EVar type var_name i, env')
 
-  | P4cub.Expr.EHeaderStack fields headers size next_index i => 
-    let type := CubE.TBool in 
-    let '((hdrs_stmt, hdrs'), env_hdrs) := TranslateExprList headers env i in 
+  | P4cub.Expr.EHeaderStack fields headers size next_index i => (*TODO: get the correct type*)
+    let type := P4cub.Expr.TBool in 
+    let '((hdrs_stmt, hdrs'), env_hdrs) := TransformExprList headers env i in 
     let (var_name, env') := VarNameGen.new_var env_hdrs in
-    let declaration := SelS.SVardecl type var_name i in
-    let assign := SelS.SHeaderStack fields hdrs' size next_index var_name i in 
-    let stmt := SelS.SSeq hdrs_stmt (SelS.SSeq declaration assign i) i in
-    ((stmt, SelE.EVar type var_name i), env')
+    let declaration := P4cub.Stmt.SVardecl type var_name i in
+    let assign := P4cub.Stmt.SAssign type (P4cub.Expr.EVar type var_name i) (P4cub.Expr.EHeaderStack fields hdrs' size next_index i) i in 
+    let stmt := P4cub.Stmt.SSeq hdrs_stmt (P4cub.Stmt.SSeq declaration assign i) i in
+    (stmt, P4cub.Expr.EVar type var_name i, env')
 
-  | P4cub.Expr.EHeaderStackAccess stack index i => 
-    let type := CubE.TBool in 
-    let '((stack_stmt, stack'), env_stack) := TransformExpr stack env in
-    let val := SelE.EHeaderStackAccess stack' index i in 
+  | P4cub.Expr.EHeaderStackAccess stack index i =>(*TODO: get the correct type*) 
+    let type := P4cub.Expr.TBool in 
+    let '(stack_stmt, stack', env_stack) := TransformExpr stack env in
+    let val := P4cub.Expr.EHeaderStackAccess stack' index i in 
     let stmt := stack_stmt in
-    ( (stmt, val), env_stack) *)
-  (*| CubE.EString s i => ((SelS.SSkip i,SelE.EString _ s i), env)
-  | CubE.EEnum x m i => ((SelS.SSkip i, SelE.EEnum _ x m i), env)*)
-  | _ => (P4cub.Stmt.SSkip tags_dummy, P4cub.Expr.EBool true tags_dummy, env)
+    (stmt, val, env_stack)
+  (*| P4cub.Expr.EString s i => ((P4cub.Stmt.SSkip i,P4cub.Expr.EString _ s i), env)
+  | P4cub.Expr.EEnum x m i => ((P4cub.Stmt.SSkip i, P4cub.Expr.EEnum _ x m i), env)*)
   end.
 
 
 
 Definition TranslateCArg 
-(carg: P4cub.Expr.constructor_arg tags_t) (env: VarNameGen.t) (i : tags_t)
-: st * P4cub.Expr.constructor_arg tags_t * VarNameGen.t.
-  Admitted.
+  (carg: P4cub.Expr.constructor_arg tags_t) (env: VarNameGen.t) (i : tags_t)
+  : st * P4cub.Expr.constructor_arg tags_t * VarNameGen.t :=
+  match carg with 
+  | P4cub.Expr.CAExpr expr => 
+    let '(stmt_expr, expr', env_expr) := TransformExpr expr env in
+    (stmt_expr, P4cub.Expr.CAExpr expr', env_expr)
+  | P4cub.Expr.CAName x => (P4cub.Stmt.SSkip i, P4cub.Expr.CAName x, env)
+  end.
 Definition TranslateCArgs 
-(cargs: P4cub.Expr.constructor_args tags_t) (env: VarNameGen.t) (i: tags_t)
-: st * P4cub.Expr.constructor_args tags_t * VarNameGen.t.
-  Admitted.
+  (cargs: P4cub.Expr.constructor_args tags_t) (env: VarNameGen.t) (i: tags_t)
+  : st * P4cub.Expr.constructor_args tags_t * VarNameGen.t :=
+  Field.fold 
+  (fun (name : string)
+       (arg: P4cub.Expr.constructor_arg tags_t)
+       (cumulator : (P4cub.Stmt.s tags_t) * (P4cub.Expr.constructor_args tags_t) * VarNameGen.t)
+   => 
+   let '(prev_stmt, prev_cargs, prev_env) := cumulator in 
+   let '(arg_stmt, arg', arg_env) := TranslateCArg arg prev_env i in 
+   let new_stmt := P4cub.Stmt.SSeq prev_stmt arg_stmt i in
+   let new_cargs := prev_cargs ++ [(name,arg')] in
+   (new_stmt, new_cargs, arg_env) 
+  ) cargs (P4cub.Stmt.SSkip i, [], env).
 
 Definition TranslateArgs (arguments : P4cub.Expr.args tags_t) (env : VarNameGen.t) (i: tags_t)
-  : st * P4cub.Expr.args tags_t * VarNameGen.t.
-  Admitted.
+  : st * P4cub.Expr.args tags_t * VarNameGen.t :=
+  Field.fold 
+  (fun (name : string) 
+       (arg: P4cub.paramarg (P4cub.Expr.t * P4cub.Expr.e tags_t) (P4cub.Expr.t * P4cub.Expr.e tags_t))
+       (cumulator: ((P4cub.Stmt.s tags_t * (P4cub.Expr.args tags_t) * VarNameGen.t)))
+      =>  
+      let '((prev_stmt, prev_args), prev_env) := cumulator in 
+      let (type, expr) := match arg with
+        | P4cub.PADirLess (t, e)
+        | P4cub.PAIn (t, e)
+        | P4cub.PAOut (t, e)
+        | P4cub.PAInOut (t, e) => (t, e)
+        end in
+      let '((new_stmt, new_expr), new_env):= TransformExpr expr prev_env in
+      let new_stmt := P4cub.Stmt.SSeq prev_stmt new_stmt i in
+      let new_arg := match arg with 
+        | P4cub.PADirLess _ => P4cub.PAIn (type, new_expr)
+        | P4cub.PAIn _ => P4cub.PAIn (type, new_expr)
+        | P4cub.PAOut _ => P4cub.PAOut (type, new_expr)
+        | P4cub.PAInOut _ => P4cub.PAInOut (type, new_expr) 
+        end in
+      ((new_stmt, prev_args++[(name,new_arg)]), new_env)
+  ) arguments ((P4cub.Stmt.SSkip i, []), env).
+
 
 Definition TranslateArrowE (arguments : P4cub.Expr.arrowE tags_t) (env : VarNameGen.t) (i: tags_t)
-  : st * P4cub.Expr.arrowE tags_t * VarNameGen.t.
-  Admitted.
+  : st * P4cub.Expr.arrowE tags_t * VarNameGen.t :=
+  match arguments with 
+  | P4cub.Arrow pas returns => 
+    let '(pas_stmt, pas', env_pas) := TranslateArgs pas env i in 
+    let '(returns_stmt, returns', env_returns):= 
+      match returns with
+      | None => ((pas_stmt, None), env_pas)
+      | Some (type, expr) => 
+        let '(expr_stmt, expr', env_expr) := TransformExpr expr env_pas in
+        (P4cub.Stmt.SSeq pas_stmt expr_stmt i, Some (type, expr'), env_expr) 
+      end in
+    (returns_stmt, P4cub.Arrow pas' returns', env_returns)
+  end.
 
 Fixpoint TranslateStatement (stmt : st) (env: VarNameGen.t) 
-  : st * VarNameGen.t.
-  Admitted.
+  : st * VarNameGen.t := 
+  match stmt with
+  | P4cub.Stmt.SHeaderStackOp name op n i => (P4cub.Stmt.SHeaderStackOp name op n i, env)
+  | P4cub.Stmt.SSkip i => (P4cub.Stmt.SSkip i, env)
+  | P4cub.Stmt.SVardecl type x i => (P4cub.Stmt.SVardecl type x i, env)
+  | P4cub.Stmt.SAssign type lhs rhs i => 
+    let '(lhs_stmt, lhs', env_lhs) := TransformExpr lhs env in 
+    let '(rhs_stmt, rhs', env_rhs) := TransformExpr rhs env_lhs in 
+    let new_stmt := P4cub.Stmt.SSeq lhs_stmt (P4cub.Stmt.SSeq rhs_stmt (P4cub.Stmt.SAssign type lhs' rhs' i) i) i in 
+    (new_stmt, env_rhs)
+  | P4cub.Stmt.SConditional guard_type guard tru_blk fls_blk i =>
+    let '(guard_stmt, guard', env_guard) := TransformExpr guard env in
+    let (tru_blk', env_tru) := TranslateStatement tru_blk env_guard in 
+    let (fls_blk', env_fls) := TranslateStatement fls_blk env_tru in 
+    let new_stmt := P4cub.Stmt.SSeq guard_stmt (P4cub.Stmt.SConditional guard_type guard' tru_blk' fls_blk' i) i in
+    (new_stmt, env) 
+  | P4cub.Stmt.SSeq s1 s2 i => 
+    let (s1', env_s1) := TranslateStatement s1 env in 
+    let (s2', env_s2) := TranslateStatement s2 env_s1 in 
+    (P4cub.Stmt.SSeq s1' s2' i, env_s2)
+  | P4cub.Stmt.SBlock block => 
+    let (block', env_block) := TranslateStatement block env in
+    (P4cub.Stmt.SBlock block' , env_block)
+  | P4cub.Stmt.SExternMethodCall e f targs args i =>
+    let '(stmt_args, args', env_args) := TranslateArrowE args env i in 
+    (P4cub.Stmt.SSeq stmt_args (P4cub.Stmt.SExternMethodCall e f targs args' i) i, env_args)
+  | P4cub.Stmt.SFunCall f targs args i => 
+    let '(stmt_args, args', env_args) := TranslateArrowE args env i in
+    (P4cub.Stmt.SSeq stmt_args (P4cub.Stmt.SFunCall f targs args' i) i, env_args)
+  | P4cub.Stmt.SActCall f args i => 
+    let '(stmt_args, args', env_args) := TranslateArgs args env i in
+    (P4cub.Stmt.SSeq stmt_args (P4cub.Stmt.SActCall f args' i) i, env_args)
+  | P4cub.Stmt.SReturnVoid i => (P4cub.Stmt.SReturnVoid i, env)
+  | P4cub.Stmt.SReturnFruit t e i => 
+    let '(e_stmt, e', env_e) := TransformExpr e env in
+    (P4cub.Stmt.SSeq e_stmt (P4cub.Stmt.SReturnFruit t e' i) i, env_e)
+  | P4cub.Stmt.SExit i => (P4cub.Stmt.SExit i, env)
+  | P4cub.Stmt.SInvoke x i => (P4cub.Stmt.SInvoke x i, env)
+  | P4cub.Stmt.SApply x ext args i => 
+    let '(stmt_args, args', env_args) := TranslateArgs args env i in 
+    (P4cub.Stmt.SSeq stmt_args (P4cub.Stmt.SApply x ext args' i) i, env_args)
+  | P4cub.Stmt.SSetValidity hdr val i  => 
+    let '(hdr_stmt, hdr', env_hdr) := TransformExpr hdr env in 
+    (P4cub.Stmt.SSeq hdr_stmt (P4cub.Stmt.SSetValidity hdr' val i) i, env_hdr)
+  end.
+
 
 Fixpoint TranslateParserExpr (expr: P4cub.Parser.e tags_t) (env : VarNameGen.t)
   : st * P4cub.Parser.e tags_t * VarNameGen.t.

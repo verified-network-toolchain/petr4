1f2d113ecd29d304cf20f26767a0ee5d8efffa35
rnp39 <rnp39@cornell.edu>
2021-12-09 17:25:01 -0800

    occurs and free variables in p4cub

--- deps/poulet4/lib/P4cub/Syntax/Occurs.v
+++ deps/poulet4/lib/P4cub/Syntax/Occurs.v
@@ -1,8 +1,79 @@
 Set Warnings "custom-entry-overridden,parsing".
-Require Import Poulet4.P4cub.Syntax.AST
+Require Export Poulet4.P4cub.Syntax.AST.
+Require Import Poulet4.P4cub.Syntax.IndPrincip
         Poulet4.P4cub.Syntax.CubNotations.
-Import String.
-Import AllCubNotations.
+Import String AllCubNotations.
+
+(** * Free Variables & Name Occurence *)
+
+Definition
+  list_of_option
+  {U V : Type}
+  (f : U -> list V)
+  (o : option U) : list V :=
+  match o with
+  | Some u => f u
+  | None   => []
+  end.
+
+Section FV.
+  Context {tags_t : Type}.
+  
+  Fixpoint FVₑ (e : Expr.e tags_t) : list String.string :=
+    match e with
+    | <{ BOOL _ @ _ }>
+    | <{ _ W _ @ _ }>
+    | <{ _ S _ @ _ }>
+    | <{ Error _ @ _ }>
+    | <{ Matchkind _ @ _ }>             => []
+    | <{ Var x:_ @ _ }>                 => [x]
+    | <{ Slice e[_:_] @ _ }>
+    | <{ Cast e:_ @ _ }>
+    | <{ UOP _ e:_ @ _}>
+    | <{ Mem e dot _ : _ @ _ }>
+    | <{ Access e[_]: _ @ _ }>          => FVₑ e
+    | <{ BOP e₁ _ e₂ : _ @ _ }>         => FVₑ e₁ ++ FVₑ e₂
+    | <{ tup es @ _ }>
+    | <{ Stack es:_ nextIndex:=_ @ _ }> => flat_map FVₑ es
+    | <{ struct { es } @ _ }>           => flat_map (fun '(_,e) => FVₑ e) es
+    | <{ hdr { es } valid := e @ _ }>
+      => FVₑ e ++ flat_map (fun '(_,e) => FVₑ e) es
+    end.
+
+  Definition
+    FV_paramarg
+    '((PAIn e
+      | PAOut e
+      | PAInOut e
+      | PADirLess e) : paramarg (Expr.e tags_t) (Expr.e tags_t))
+    : list String.string := FVₑ e.
+  
+  Definition
+    FV_arrowE
+    '({| paramargs := es; rtrns := e |} : Expr.arrowE tags_t)
+    : list String.string :=
+    list_of_option FVₑ e ++ flat_map (FV_paramarg ∘ snd) es.
+  
+  Fixpoint FVₛ (s : Stmt.s tags_t) : list String.string :=
+    match s with
+    | -{ skip @ _ }-
+    | -{ exit @ _ }-
+    | -{ invoke _ @ _ }-             => []
+    | -{ declare x : _ @ _ }-
+    | Stmt.SHeaderStackOp x _ _ _    => [x]
+    | -{ init x := e @ _ }-          => x :: FVₑ e
+    | -{ asgn e₁ := e₂ @ _ }-        => FVₑ e₁ ++ FVₑ e₂
+    | -{ if e then s₁ else s₂ @ _ }- => FVₑ e ++ FVₛ s₁ ++ FVₛ s₂
+    | -{ s₁; s₂ @ _ }-               => FVₛ s₁ ++ FVₛ s₂
+    | -{ b{ s }b }-                  => FVₛ s
+    | Stmt.SExternMethodCall _ _ _ arr _
+    | Stmt.SFunCall _ _ arr _        => FV_arrowE arr
+    | -{ calling _ with es @ _ }-
+    | -{ apply _ with _ & es @ _ }-  => flat_map (FV_paramarg ∘ snd) es
+    | -{ return e @ _ }-             => list_of_option FVₑ e
+    | Stmt.SSetValidity e _ _        => FVₑ e
+    end.
+End FV.
 
 (* TODO: induction hypothesis. *)
 
@@ -12,82 +83,121 @@ Section Occurs.
   Variable x : string.
 
   (** A variable [x] Occurs in an expression. *)
-  Inductive Occurs_e : Expr.e tags_t -> Prop :=
+  Inductive Occursₑ : Expr.e tags_t -> Prop :=
   | Occurs_var τ i :
-      Occurs_e <{ Var x:τ @ i }>
+      Occursₑ <{ Var x:τ @ i }>
   | Occurs_slice e hi lo i :
-      Occurs_e e ->
-      Occurs_e <{ Slice e [hi:lo] @ i }>
+      Occursₑ e ->
+      Occursₑ <{ Slice e [hi:lo] @ i }>
   | Occurs_cast τ e i :
-      Occurs_e e ->
-      Occurs_e <{ Cast e:τ @ i }>
+      Occursₑ e ->
+      Occursₑ <{ Cast e:τ @ i }>
   | Occurs_uop τ op e i :
-      Occurs_e e ->
-      Occurs_e <{ UOP op e:τ @ i }>
+      Occursₑ e ->
+      Occursₑ <{ UOP op e:τ @ i }>
   | Occurs_bop τ op e₁ e₂ i :
-      Occurs_e e₁ \/ Occurs_e e₂ ->
-      Occurs_e <{ BOP e₁ op e₂ : τ @ i }>
+      Occursₑ e₁ \/ Occursₑ e₂ ->
+      Occursₑ <{ BOP e₁ op e₂ : τ @ i }>
   | Occurs_tuple es i :
-      Exists Occurs_e es ->
-      Occurs_e <{ tup es @ i }>
+      Exists Occursₑ es ->
+      Occursₑ <{ tup es @ i }>
   | Occurs_struct es i :
-      Exists (Occurs_e ∘ snd) es ->
-      Occurs_e <{ struct { es } @ i }>
+      Exists (Occursₑ ∘ snd) es ->
+      Occursₑ <{ struct { es } @ i }>
   | Occurs_header es e i :
-      Exists (Occurs_e ∘ snd) es \/ Occurs_e e ->
-      Occurs_e <{ hdr { es } valid:=e @ i }>
-  | Occurs_member τ e x i :
-      Occurs_e e ->
-      Occurs_e <{ Mem e dot x : τ @ i }>
+      Exists (Occursₑ ∘ snd) es \/ Occursₑ e ->
+      Occursₑ <{ hdr { es } valid:=e @ i }>
+  | Occurs_member τ e y i :
+      Occursₑ e ->
+      Occursₑ <{ Mem e dot y : τ @ i }>
   | Occurs_stack τs es ni i :
-      Exists Occurs_e es ->
-      Occurs_e <{ Stack es:τs nextIndex:=ni @ i }>
+      Exists Occursₑ es ->
+      Occursₑ <{ Stack es:τs nextIndex:=ni @ i }>
   | Occurs_access τs e n i :
-      Occurs_e e ->
-      Occurs_e <{ Access e[n]:τs @ i }>.
-
+      Occursₑ e ->
+      Occursₑ <{ Access e[n]:τs @ i }>.
 
   (** [x] occurs in call arguments. *)
   Definition
     Occurs_arrowE
     '({| paramargs := es; rtrns := e |} : Expr.arrowE tags_t) : Prop :=
-    Exists (pred_paramarg_same Occurs_e ∘ snd) es \/
+    Exists (pred_paramarg_same Occursₑ ∘ snd) es \/
     match e with
-    | Some e => Occurs_e e
+    | Some e => Occursₑ e
     | None   => False
     end.
   
   (** A variable [x] Occurs in a statement. *)
-  (* TODO: control, parser apply, etc. *)
-  Inductive Occurs_s : Stmt.s tags_t -> Prop :=
+  Inductive Occursₛ : Stmt.s tags_t -> Prop :=
   | Occurs_vardecl y et i :
       match et with
       | inl _ => x = y
-      | inr e => x = y \/ Occurs_e e
+      | inr e => x = y \/ Occursₑ e
       end ->
-      Occurs_s -{ var x with et @ i }-
+      Occursₛ -{ var x with et @ i }-
   | Occurs_assign e₁ e₂ i :
-      Occurs_e e₁ \/ Occurs_e e₂ ->
-      Occurs_s -{ asgn e₁ := e₂ @ i }-
+      Occursₑ e₁ \/ Occursₑ e₂ ->
+      Occursₛ -{ asgn e₁ := e₂ @ i }-
   | Occurs_cond e s₁ s₂ i :
-      Occurs_e e \/ Occurs_s s₁ \/ Occurs_s s₂ ->
-      Occurs_s -{ if e then s₁ else s₂ @ i }-
-  | Occurs_seq s₁ s₂ i :
-      Occurs_s s₁ \/ Occurs_s s₂ ->
-      Occurs_s -{ s₁; s₂ @ i }-
+      Occursₑ e \/ Occursₛ s₁ \/ Occursₛ s₂ ->
+      Occursₛ -{ if e then s₁ else s₂ @ i }-
+  | Occursₛeq s₁ s₂ i :
+      Occursₛ s₁ \/ Occursₛ s₂ ->
+      Occursₛ -{ s₁; s₂ @ i }-
   | Occurs_block s :
-      Occurs_s s ->
-      Occurs_s -{ b{ s }b }-
+      Occursₛ s ->
+      Occursₛ -{ b{ s }b }-
   | Occurs_return e i :
-      Occurs_e e ->
-      Occurs_s (Stmt.SReturn (Some e) i)
-  | Occurs_set_validity e b i :
-    Occurs_e e ->
-    Occurs_s (Stmt.SSetValidity e b i)
-  | Occurs_extern_method_call e m τs args i :
+      Occursₑ e ->
+      Occursₛ (Stmt.SReturn (Some e) i)
+  | Occursₛet_validity e b i :
+    Occursₑ e ->
+    Occursₛ (Stmt.SSetValidity e b i)
+  | Occursₑxtern_method_call e m τs args i :
       Occurs_arrowE args ->
-      Occurs_s (Stmt.SExternMethodCall e m τs args i)
-  | occurs_fun_call f τs args i :
+      Occursₛ (Stmt.SExternMethodCall e m τs args i)
+  | Occurs_fun_call f τs args i :
       Occurs_arrowE args ->
-      Occurs_s (Stmt.SFunCall f τs args i).
+      Occursₛ (Stmt.SFunCall f τs args i)
+  | Occurs_act_call a es i :
+      Exists (pred_paramarg_same Occursₑ ∘ snd) es ->
+      Occursₛ (Stmt.SActCall a es i)
+  | Occurs_apply y exts es i :
+      Exists (pred_paramarg_same Occursₑ ∘ snd) es ->
+      Occursₛ (Stmt.SApply y exts es i)
+  | Occursₛtack_op o n i :
+      Occursₛ (Stmt.SHeaderStackOp x o n i).
+
+  Section FV_Occurs.
+    Hint Rewrite in_app_iff : core.
+    
+    Lemma Occursₑ_FVₑ : forall e, Occursₑ e -> In x (FVₑ e).
+    Proof.
+      intros e Hoe;
+        induction e as
+          [ b i
+          | w n i
+          | w z i
+          | t y i
+          | e hi lo i IHe
+          | t e i IHe
+          | t o e IHe
+          | t op e1 e2 i IHe1 IHe2
+          | es i IHes
+          | es i IHes
+          | es e i IHe IHes
+          | t y e i IHe
+          | err i
+          | mk i
+          | ts es n i IHes
+          | t e n i IHe
+          ] using custom_e_ind
+        ; inv Hoe; unravel in *;
+          autorewrite with core; intuition.
+      - rewrite Forall_forall in IHes.
+        rewrite Exists_exists in H0.
+        destruct H0 as (e & HInes & Hoe).
+        rewrite in_flat_map; eauto.
+    Admitted.
+  End FV_Occurs.
 End Occurs.

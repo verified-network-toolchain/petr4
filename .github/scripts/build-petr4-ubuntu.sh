80ab2e376d2ef0a879b5b3fffd984ee322159013
pataei <paris.ataei@gmail.com>
2021-12-10 16:45:47 -0500

    getting list of deps

--- .github/scripts/build-petr4-ubuntu.sh
+++ .github/scripts/build-petr4-ubuntu.sh
@@ -19,9 +19,9 @@ opam upgrade
 #opam switch 4.09.1
 opam pin add p4pp https://github.com/cornell-netlab/p4pp.git
 #install dune
-opam install dune
+#opam install dune
 cd ../..
-#opam external-lib-deps --missing @install
+dune external-lib-deps --missing @install
 
 # build petr4
 dune build

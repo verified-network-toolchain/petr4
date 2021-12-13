d8ff22f4345986df4a3c9f39ab627411523cc771
Parisa Ataei <paris.ataei@gmail.com>
2021-12-13 17:19:56 -0500

    Create build-poulet4-macos.sh

---
+++ .github/scripts/build-poulet4-macos.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# petr4 build script for macos
+
+set -e  # Exit on error.
+set -x  # Make command execution verbose
+
+export PETR4_DEPS="m4 \
+                   gmp"
+
+
+# install dependencies
+brew update
+brew install \
+  ${PETR4_DEPS}
+opam update
+opam upgrade
+# install p4pp
+#opam switch 4.09.1
+opam pin add p4pp https://github.com/cornell-netlab/p4pp.git
+eval $(opam env)
+#install dune
+#opam install dune
+#export PATH="/usr/local/opt/dune/bin:$PATH"
+cd ../..
+#dune external-lib-deps --missing @install
+opam install ANSITerminal alcotest bignum cstruct-sexp pp ppx_deriving ppx_deriving_yojson yojson js_of_ocaml js_of_ocaml-lwt js_of_ocaml-ppx
+#dune external-lib-deps --missing @@default
+
+# build petr4
+#dune init project petr4
+dune build --profile release
+dune install
+
+

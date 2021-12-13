1057bb227871ec9b37aafd348bc95c7a10663ed3
Eric Campbell <1234goal@gmail.com>
2021-12-13 12:05:58 -0500

    Add MultiProtocol example; implement switch statements in p4light -> p4cub translation

--- deps/poulet4/lib/ToP4cubTest.v
+++ deps/poulet4/lib/ToP4cubTest.v
@@ -2,14 +2,15 @@ Require Import Poulet4.P4defs.
 Require Import Poulet4.Syntax.
 Require Import Poulet4.LightExamples.SimpleNat.
 Require Import Poulet4.LightExamples.ECMP2.
+Require Import Poulet4.LightExamples.MultiProtocol.
 Require Import Poulet4.P4cub.Util.Result.
 Require Import Poulet4.ToP4cub.
 
 Import Result Syntax List ListNotations.
 
 (* The Test Programs*)
-Import SimpleNat.
 
+(* Test Lemma For simple_nat.p4*)
 (* Compute (translate_program Info NoInfo SimpleNat.prog). *)
 Lemma simplenat_no_error: Result.is_ok (translate_program Info NoInfo SimpleNat.prog).
 Proof.
@@ -17,14 +18,23 @@ Proof.
   trivial.
 Qed.
 
-Import ECMP2.
-
 (* Compute ECMP2.prog. *)
 
+(* Test lemma for ecmp_2.v *)
 (* Compute (translate_program Info NoInfo ECMP2.prog). *)
 (* CAVEAT EMPTOR.  I swapped all the "selector" matchkinds in ECMP2.prog for "exact" *)
 (* TODO When we add support for arbitrary matchkinds, regenerate ECMP2 so that it uses "selector".  *)
 Lemma ecmp2_no_error: Result.is_ok (translate_program Info NoInfo ECMP2.prog).
+Proof.
+  compute.
+  trivial.
+Qed.
+
+
+Compute (translate_program Info NoInfo MultiProtocol.prog).
+
+Lemma multiprotocol_no_error :
+  Result.is_ok (translate_program Info NoInfo MultiProtocol.prog).
 Proof.
   compute.
   trivial.

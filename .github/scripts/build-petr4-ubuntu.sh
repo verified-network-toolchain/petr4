455dddd0183cd72dd199dccec4aca78fcbae3ec1
pataei <paris.ataei@gmail.com>
2021-12-14 09:54:51 -0800

    clean up

--- .github/scripts/build-petr4-ubuntu.sh
+++ .github/scripts/build-petr4-ubuntu.sh
@@ -20,6 +20,7 @@ export PETR4_DEPS_OPAM="ANSITerminal \
                         js_of_ocaml \
                         js_of_ocaml-lwt \
                         js_of_ocaml-ppx"
+
 # install deps
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
@@ -30,17 +31,11 @@ opam upgrade
 #opam switch 4.09.1
 opam pin add p4pp https://github.com/cornell-netlab/p4pp.git
 eval $(opam env)
-#install dune
-#opam install dune
-export PATH="/usr/local/opt/dune/bin:$PATH"
+#export PATH="/usr/local/opt/dune/bin:$PATH"
 #dune external-lib-deps --missing @install
-#opam install ANSITerminal alcotest bignum cstruct-sexp pp ppx_deriving ppx_deriving_yojson yojson js_of_ocaml js_of_ocaml-lwt js_of_ocaml-ppx
 opam install \
   ${PETR4_DEPS_OPAM}
-#dune external-lib-deps --missing @@default
 
-#cd ../..
 # build petr4
-#dune init project petr4
 dune build --profile release
 dune install

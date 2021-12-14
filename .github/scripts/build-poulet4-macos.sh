5084a6f9315f9f3129ab93ca836dbd18cf23fad8
pataei <paris.ataei@gmail.com>
2021-12-14 10:03:14 -0800

    using Ryan version of coq and ocaml

--- .github/scripts/build-poulet4-macos.sh
+++ .github/scripts/build-poulet4-macos.sh
@@ -8,6 +8,24 @@ set -x  # Make command execution verbose
 export PETR4_DEPS="m4 \
                    gmp"
 
+export PETR4_DEPS_OPAM="ANSITerminal \
+                        alcotest \
+                        bignum \
+                        cstruct-sexp \
+                        pp \
+                        ppx_deriving \
+                        ppx_deriving_yojson \
+                        yojson \
+                        js_of_ocaml \
+                        js_of_ocaml-lwt \
+                        js_of_ocaml-ppx"
+
+export POULET4_DEPS="coq-equations \
+                     coq-record-update \
+                     coq-compcert "
+
+export POULET4_CCOMP_DEPS="zarith"
+
 pwd
 # install dependencies
 brew update
@@ -16,26 +34,22 @@ brew install \
 opam update
 opam upgrade
 # install p4pp
-#opam switch 4.09.1
+opam switch 4.12.0
 opam pin add p4pp https://github.com/cornell-netlab/p4pp.git
 eval $(opam env)
-#install dune
-#opam install dune
-#export PATH="/usr/local/opt/dune/bin:$PATH"
-#cd ../..
-#dune external-lib-deps --missing @install
-# install dependencies for petr4
-opam install ANSITerminal alcotest bignum cstruct-sexp pp ppx_deriving ppx_deriving_yojson yojson js_of_ocaml js_of_ocaml-lwt js_of_ocaml-ppx
-#dune external-lib-deps --missing @@default
-
-pwd
 
 # install deps for poulet4
-opam pin add coq $NEW_VERSION
+opam pin add coq 8.13.2
 opam repo add coq-released https://coq.inria.fr/opam/released
-opam install coq-equations coq-record-update coq-compcert 
+# install dependencies for petr4, poulet4, poulet4_ccomp
+opam install \
+  ${PETR4_DEPS_OPAM} \
+  ${POULET4_DEPS} \
+  ${POULET4_CCOMP_DEPS}
+#opam install coq-equations coq-record-update coq-compcert 
 # install deps for poulet4_ccomp
-opam install zarith
+#opam install zarith
+
 make
 make install
 

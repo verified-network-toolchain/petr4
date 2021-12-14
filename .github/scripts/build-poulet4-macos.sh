83ed1ddc83d8714e364c052dcf65745560620df0
pataei <paris.ataei@gmail.com>
2021-12-13 18:09:41 -0800

    fixing deps for poulet4

--- .github/scripts/build-poulet4-macos.sh
+++ .github/scripts/build-poulet4-macos.sh
@@ -31,7 +31,9 @@ opam install ANSITerminal alcotest bignum cstruct-sexp pp ppx_deriving ppx_deriv
 pwd
 
 # install deps for poulet4
-opam install coq coq-equations coq-record-update coq-compcert 
+opam pin add coq 8.14.0
+opam repo add coq-released https://coq.inria.fr/opam/released
+opam install coq-equations coq-record-update coq-compcert 
 # install deps for poulet4_ccomp
 opam install zarith
 make

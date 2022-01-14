c8de1dc2d54dc49c4ef28c6a89cf34883bf63ed3
Shengyi Wang <txyyss@gmail.com>
2022-01-15 05:45:09 +0800

    More cases in interp_expr_safe are proved.

--- deps/poulet4/lib/InterpreterSafe.v
+++ deps/poulet4/lib/InterpreterSafe.v
@@ -1,9 +1,17 @@
+Require Import Coq.micromega.Lia.
 Require Import Poulet4.Syntax.
+Require Import Poulet4.SyntaxUtil.
+Require Import Poulet4.ValueUtil.
 Require Import Poulet4.ExprInd.
 Require Poulet4.Semantics.
 Require Poulet4.Interpreter.
 Check @Interpreter.interp_expr.
 Print Interpreter.interp_expr.
+
+Ltac destruct_match H :=
+  match goal with
+  | H: context [match ?A with | _ => _ end] |- _ => destruct A eqn:?H
+  end.
 
 Section InterpreterSafe.
   Context {tags_t: Type} {tag: Sublist.Inhabitant tags_t}.
@@ -13,7 +21,7 @@ Section InterpreterSafe.
   Theorem interp_expr_safe:
     forall expr this st sv,
       Interpreter.interp_expr ge this st expr = Some sv ->
-      Semantics.exec_expr ge ValueUtil.read_ndetbit this st expr sv.
+      Semantics.exec_expr ge read_ndetbit this st expr sv.
   Proof.
     induction expr using expr_ind; intros.
     - cbv in H.
@@ -30,28 +38,40 @@ Section InterpreterSafe.
       inversion H.
       subst sv.
       constructor.
-    - cbn in H. destruct (Semantics.is_directional dir) eqn:?H.
+    - cbn in H. destruct_match H.
       + constructor; auto.
       + now apply Semantics.exec_expr_name_const.
     - cbn in H. destruct (Interpreter.interp_expr ge this st expr2) eqn:?H;
         unfold Option.option_bind in H. 2: inversion H.
-      destruct (Semantics.array_access_idx_to_z
-                  (Interpreter.interp_sval_val v)) eqn: ?H. 2: inversion H.
-      destruct (Interpreter.interp_expr ge this st expr1) eqn:?H. 2: inversion H.
-      destruct v0; try now inversion H.
-      destruct (Semantics.get_real_type ge typ) eqn:?H. 2: inversion H.
-      destruct (ValueUtil.uninit_sval_of_typ None p) eqn:?H. 2: inversion H.
-      inversion H; subst; clear H. econstructor; eauto. red. clear H0.
-      induction v; simpl in H1; try now inversion H1.
+      destruct_match H. 2: inversion H. destruct_match H. 2: inversion H.
+      destruct v0; try now inversion H. destruct_match H. 2: inversion H.
+      destruct_match H; inversion H; subst; clear H. econstructor; eauto. red.
+      clear H0. induction v; simpl in H1; try now inversion H1.
       all: simpl; constructor.
       + clear. induction value; simpl; constructor; auto.
         unfold Interpreter.bit_init. destruct a; constructor.
       + clear. induction value; simpl; constructor; auto.
         unfold Interpreter.bit_init. destruct a; constructor.
       + apply IHv; auto.
-    - cbn in H.
-    - admit.
-    - admit.
+    - cbn in H. destruct (Interpreter.interp_expr ge this st expr) eqn:?H;
+        unfold Option.option_bind in H. 2: inversion H.
+      destruct_match H. 2: inversion H. destruct p.
+      destruct n. 1: rewrite Bool.andb_false_r in H; inversion H.
+      destruct_match H; inversion H; subst; clear H. econstructor; eauto.
+      apply andb_prop in H2. destruct H2. apply PeanoNat.Nat.leb_le in H, H2. lia.
+    - cbn in H0. destruct
+        (lift_option (List.map (Interpreter.interp_expr ge this st) vs)) eqn:?H;
+        unfold Option.option_bind in H0; inversion H0. subst; clear H0.
+      constructor. revert l H1. induction H; intros.
+      + simpl in H1. inversion H1. constructor.
+      + destruct l0; simpl in H1; destruct_match H1; try now inversion H1.
+        all: destruct_match H1; inversion H1; subst. constructor; auto.
+    - cbn in H0. destruct (lift_option_kv (AList.map_values (Interpreter.interp_expr ge this st) es)) eqn:?H; unfold Option.option_bind in H0; inversion H0. subst. clear H0.
+      constructor. revert l H1. induction H; intros.
+      + simpl in H1. inversion H1. simpl. constructor.
+      + destruct l0; simpl in H1; destruct x; destruct_match H1; try now inversion H1.
+        all: destruct_match H1; inversion H1; subst. clear H1.
+        simpl. destruct t0. simpl. constructor; auto. now apply IHForall.
     - admit.
     - admit.
     - admit.

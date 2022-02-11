d7101eb0eb08e52f4ed2212a625b51f7601f20d2
pataei <paris.ataei@gmail.com>
2022-02-10 20:58:48 -0500

    fix syntax err

--- .github/scripts/typecheck-petr4-vs-p4c.sh
+++ .github/scripts/typecheck-petr4-vs-p4c.sh
@@ -3,27 +3,6 @@
 # set -e # Exit on error.
 set -x # Make command execution verbose
 
-#############
-# typecheck all tests with petr4 and write results
-# petr4 typecheck -I <path to core and etc> <p4 file with full path>
-# p4test -I <path to core and etc> --top4 "" <p4 file with full path>
-# for file in /petr4/p4cgit/p4c/testdata/
-# do
-#   petr4 typecheck -I /petr4/p4cgit/p4c/p4include/ "$file" > "/petr4/results/petr4-results/${file%*}.out"
-#   p4test -I /petr4/p4cgit/p4c/p4include/ --top4 "" "$file" > "/petr4/results/p4c-results/${file%*}.out"
-# done
-
-# # find /p4cgit/p4c/testdata/ -type p4 -exec command {options} {} \; > results.out
-
-# find /petr4/p4cgit/p4c/testdata/ -name "*.p4" -exec petr4 typecheck -I /petr4/p4cgit/p4c/p4include/ ${0} \; > /petr4/results/petr4-results/${0}.out
-
-# find /petr4/p4cgit/p4c/testdata/ -exec p4test -I /petr4/p4cgit/p4c/p4include/ --top4 "" {} \; > /petr4/results/p4c-results.out
-
-# find /petr4/examples/checker_tests/excluded -name "*.p4" -exec petr4 typecheck -I /petr4/examples {} \; -exec p4test -I /petr4/examples --top4 "" {} \; > /petr4/results/p4c-results.out
-
-# find /petr4/examples/checker_tests/good/ -name "*.p4" | xargs -t petr4 typecheck -I /petr4/examples/
-##########
-
 #finds all p4 files in the given directory and does stuff to them
 for file in $(find /petr4/ci-test/type-checking/testdata/p4_16_samples -name '*.p4' ! -name 'ipv*' ! -name 'tunneling_ubpf.p4' ! -name 'simple-actions_ubpf.p4' ! -name 'simple-firewall_ubpf.p4')
 do
@@ -49,48 +28,5 @@ do
   # echo "************************\n******** p4c type checking result: ********\n************************\n" >> "/petr4/ci-test/type-checking/result/lookinto/${file##*/}.out"
   ## echo "$p4c_type" >> "/petr4/ci-test/type-checking/result/lookinto/${file##*/}.out"
   # p4test -I /petr4/ci-test/type-checking/p4include --top4 "" "$file" | tee -a -i "/petr4/ci-test/type-checking/result/lookinto/${file##*/}.out"
-  ##########
-  #TODO: add the following later!
-#   # creates the expectation dir. 
-#   if [ $petr4_type == $p4c_type ]
-#   then 
-#     cp $file /petr4/ci-test/type-checking/expectation/matched
-#   else 
-#   	cp $file /petr4/ci-test/type-checking/expectation/not-matched
-#   fi
-#   # locates files in mathced or not-matched folders.
-#   if [ $petr4_type == $p4c_type ]
-#   then 
-#     cp $file /petr4/ci-test/type-checking/result/matched
-#   else 
-#   	cp $file /petr4/ci-test/type-checking/result/not-matched
-#   fi
-##########
 done
 
-##########
-# # compares result and expection 
-# expec_match_minus_res_match = $(find "$/petr4/ci-test/type-checking/expectation/matched/" "$/petr4/ci-test/type-checking/result/matched/" "$/petr4/ci-test/type-checking/result/matched/" -printf '%P\n' | sort | uniq -u)
-# expec_notmatch_minus_res_match = $(find "$/petr4/ci-test/type-checking/expectation/not-matched/" "$/petr4/ci-test/type-checking/result/not-matched/" "$/petr4/ci-test/type-checking/result/not-matched/" -printf '%P\n' | sort | uniq -u)
-# echo "match is: \n" 
-# echo "$expec_match_minus_res_match"
-# echo "not-matched is: \n"
-# echo "$expec_notmatch_minus_res_match"
-# if [[ -n $expec_match_minus_res_match ]]
-# then 
-#   echo "matched expectation - result is not empty. here are the troublesome files: \n" 
-#   echo "$expec_match_minus_res_match"
-#   exit 1
-# else 
-#   if [[ -n $expec_notmatch_minus_res_match ]]
-#   then 
-#     echo "not-matched expectation - result is not empty. here are the troublesome files: \n" 
-#     echo "$expec_notmatch_minus_res_match"
-#     exit 1
-#   else 
-#     echo "EVERYTHING CHECKED OUT! YOOHOO!"
-#   fi
-# fi
-
-# #find "$/examples/checker_tests/bad/" "$/examples/checker_tests/good/" "$/examples/checker_tests/good/" -printf '%P\n' | sort | uniq -u
-##########

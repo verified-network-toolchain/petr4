901aceb729aa52f1f5e542d28ac5856e58ed4603
pataei <paris.ataei@gmail.com>
2022-02-14 11:57:42 -0500

    move to result

--- .github/scripts/typecheck-petr4-vs-p4c.sh
+++ .github/scripts/typecheck-petr4-vs-p4c.sh
@@ -3,8 +3,8 @@
 # set -e # Exit on error.
 set -x # Make command execution verbose
 
-mkdir /petr4/ci-test/type-checking/expectation/matched 
-mkdir /petr4/ci-test/type-checking/expectation/not-matched 
+# mkdir /petr4/ci-test/type-checking/expectation/matched 
+# mkdir /petr4/ci-test/type-checking/expectation/not-matched 
 
 #finds all p4 files in the given directory and does stuff to them
 for file in $(find /petr4/ci-test/type-checking/testdata/p4_16_samples -name '*.p4' ! -name 'ipv*' ! -name 'tunneling_ubpf.p4' ! -name 'simple-actions_ubpf.p4' ! -name 'simple-firewall_ubpf.p4')
@@ -17,8 +17,8 @@ do
   # 2>&1
   p4c_type_stat=$?
   if [$petr4_type_stat eq $p4c_type_stat]
-  then cp "$file" /petr4/ci-test/type-checking/expectation/matched
-  else cp "$file" /petr4/ci-test/type-checking/expectation/not-matched
+  then cp "$file" /petr4/ci-test/type-checking/result/matched
+  else cp "$file" /petr4/ci-test/type-checking/result/not-matched
   fi
   # writes the file name, result of petr4 type checking, and p4c type checking
   # to a new file in res directory. 

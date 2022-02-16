6fee8ad08e37e7a07ef85818fbde62ce53eed474
pataei <paris.ataei@gmail.com>
2022-02-15 22:14:15 -0500

    ftix typo

--- .github/scripts/typecheck-petr4-vs-p4c.sh
+++ .github/scripts/typecheck-petr4-vs-p4c.sh
@@ -9,7 +9,7 @@ set -x # Make command execution verbose
 # ls -la
 # echo "foo bar baz" > bigOOF2
 
-finds all p4 files in the given directory and does stuff to them
+# finds all p4 files in the given directory and does stuff to them
 for file in $(find /petr4/ci-test/type-checking/testdata/p4_16_samples -name '*.p4' ! -name 'ipv*' ! -name 'tunneling_ubpf.p4' ! -name 'simple-actions_ubpf.p4' ! -name 'simple-firewall_ubpf.p4')
 do
   # gets the result of type checking from petr4 and p4c, stores them in

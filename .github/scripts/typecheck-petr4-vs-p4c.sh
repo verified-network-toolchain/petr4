a97fe3fe4585a40967a14937347c091d52ad5e3a
pataei <paris.ataei@gmail.com>
2022-02-23 20:40:25 -0500

    still fix extension

--- .github/scripts/typecheck-petr4-vs-p4c.sh
+++ .github/scripts/typecheck-petr4-vs-p4c.sh
@@ -26,7 +26,8 @@ do
   # gets the result of type checking from petr4 and p4c, stores them in
   # variables and compares them
   file1=${file##*/}
-  file2=${file1%'.out'}
+  file2=${file1%'.p4'}
+  file3="${file2}.out"
   petr4_type=$(petr4 typecheck -I /petr4/ci-test/type-checking/p4include "$file")
   petr4_type_stat=$?
   p4c_type=$(p4test -I /petr4/ci-test/type-checking/p4include "$file")
@@ -46,31 +47,31 @@ do
   fi
   # # writes the file name, result of petr4 type checking, and p4c type checking
   # # to a new file in res directory. 
-  echo "$file" > "ci-test/type-checking/expectation/lookinto/${file2}"
-  echo "\n" >> "ci-test/type-checking/expectation/lookinto/${file2}"
-  cat $file >> "ci-test/type-checking/expectation/lookinto/${file2}"
-  echo "************************\n******** petr4 type checking result: ********\n************************\n" >> "ci-test/type-checking/expectation/lookinto/${file2}"
-  petr4 typecheck -I /petr4/ci-test/type-checking/p4include "$file" | tee -a -i "ci-test/type-checking/result/lookinto/${file2}"
-  # echo "$petr4_type" >> "ci-test/type-checking/expectation/lookinto/${file2}"
-  echo "************************\n******** p4c type checking result: ********\n************************\n" >> "ci-test/type-checking/expectation/lookinto/${file2}"
-  echo "$p4c_type" >> "ci-test/type-checking/expectation/lookinto/${file2}"
-  # p4test -I /petr4/ci-test/type-checking/p4include "$file" | tee -a -i "ci-test/type-checking/result/lookinto/${file2}"
+  echo "$file" > "ci-test/type-checking/expectation/lookinto/${file3}"
+  echo "\n" >> "ci-test/type-checking/expectation/lookinto/${file3}"
+  cat $file >> "ci-test/type-checking/expectation/lookinto/${file3}"
+  echo "************************\n******** petr4 type checking result: ********\n************************\n" >> "ci-test/type-checking/expectation/lookinto/${file3}"
+  petr4 typecheck -I /petr4/ci-test/type-checking/p4include "$file" | tee -a -i "ci-test/type-checking/result/lookinto/${file3}"
+  # echo "$petr4_type" >> "ci-test/type-checking/expectation/lookinto/${file3}"
+  echo "************************\n******** p4c type checking result: ********\n************************\n" >> "ci-test/type-checking/expectation/lookinto/${file3}"
+  echo "$p4c_type" >> "ci-test/type-checking/expectation/lookinto/${file3}"
+  # p4test -I /petr4/ci-test/type-checking/p4include "$file" | tee -a -i "ci-test/type-checking/result/lookinto/${file3}"
   # # mv "ci-test/type-checking/result/lookinto/${file##*/}" "ci-test/type-checking/result/lookinto/${file##*/}.out"
 done
 
 # # moving look into files in the corresponding directory for investigation.
-# for file in $(find ci-test/type-checking/expectation/lookinto -name '*.out.p4')
-# do 
-#   file1=${file##*/}
-#   file2=${file1%'.out.p4'}
-#   file3="${file2}.p4"
-#   test -f "ci-test/type-checking/expectation/fails/${file3}" && cp "$file" ci-test/type-checking/expectation/fails
-#   test -f "ci-test/type-checking/expectation/typechecked/${file3}" && cp "$file" ci-test/type-checking/expectation/typechecked
-#   test -f "ci-test/type-checking/expectation/p4cTypeChecked/${file3}" && cp "$file" ci-test/type-checking/expectation/p4cTypeChecked
-#   test -f "ci-test/type-checking/expectation/petr4TypeChecked/${file3}" && cp "$file" ci-test/type-checking/expectation/petr4TypeChecked
-# done
+for file in $(find ci-test/type-checking/expectation/lookinto -name '*.out')
+do 
+  file1=${file##*/}
+  file2=${file1%'.out'}
+  file3="${file2}.p4"
+  test -f "ci-test/type-checking/expectation/fails/${file3}" && cp "$file" ci-test/type-checking/expectation/fails
+  test -f "ci-test/type-checking/expectation/typechecked/${file3}" && cp "$file" ci-test/type-checking/expectation/typechecked
+  test -f "ci-test/type-checking/expectation/p4cTypeChecked/${file3}" && cp "$file" ci-test/type-checking/expectation/p4cTypeChecked
+  test -f "ci-test/type-checking/expectation/petr4TypeChecked/${file3}" && cp "$file" ci-test/type-checking/expectation/petr4TypeChecked
+done
 
-# rm -r ci-test/type-checking/expectation/lookinto
+rm -r ci-test/type-checking/expectation/lookinto
 
 # once the result has been inspected we can run petr4 and p4c again 
 # and compare the new files in matched and not-matched with the 

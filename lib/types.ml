006409ef5f5f8c87b95cc95dc1ae92faa823ccd1
pataei <paris.ataei@gmail.com>
2022-02-24 11:05:17 -0500

    refactor type info

--- lib/types.ml
+++ lib/types.ml
@@ -22,13 +22,13 @@ type 'a info = Info.t * 'a [@@deriving sexp,show,yojson]
 (* type 'a info = 
   { info: Info.t;
     typ: 'a}
-  [@@deriving sexp,show,yojson]
+[@@deriving sexp,show,yojson]
  *)
 let info (i,_) = i
 
 let info_to_yojson f (_,x) = f x
 
-(* let info_to_yojson f ty = f (info ty) *)
+(* let info_to_yojson f {info; typ} = f typ *)
 
 let info_of_yojson f json =
   match f json with
@@ -37,7 +37,7 @@ let info_of_yojson f json =
 
 (* let info_of_yojson f json =
   match f json with 
-  | Ok pre -> Ok {Info.M "<yojson"; pre}
+  | Ok pre -> Ok {info = Info.M "<yojson"; typ = pre}
   | Error x -> Error x
  *)
 module P4Int = struct

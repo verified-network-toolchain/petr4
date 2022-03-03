e61f72df12bcea4463f2764cbb168039d74cb78a
Calvin-S <19calvins@alumni.tas.tw>
2022-03-03 13:26:53 -0500

    Update P4aComp.v
    Fixed build

--- deps/poulet4/lib/P4aComp/P4aComp.v
+++ deps/poulet4/lib/P4aComp/P4aComp.v
@@ -1,4 +1,4 @@
-Require Export Poulet4.P4automata.Syntax.
+(* Require Export Poulet4.P4automata.Syntax.
 (* Require Export Poulet4.P4cub.Syntax.AST. *)
 Require Import Poulet4.Utils.FinType.
 
@@ -6,13 +6,13 @@ From Poulet4 Require Export P4cub.Syntax.AST
      P4cub.Syntax.CubNotations
      P4cub.Syntax.IndPrincip
      P4cub.Syntax.Equality
-     P4cub.Syntax.Auxilary
      P4cub.Syntax.InferMemberTypes
      P4cub.Syntax.Occurs
      P4cub.Syntax.P4Field
      P4cub.Syntax.Substitution.
 Module P4c := AST.
 Module P4a := Syntax.
+
 (* Print P4a.H. *)
 
 Section P4AComp. 
@@ -28,9 +28,9 @@ Context `{H_finite: @Finite H _ H_eq_dec}.
 
 Variable (tags_t : Type).
 
-  Fixpoint collect_hdrs_prog (prog : P4c.TopDecl.d tags_t) : list (H * nat) :=
+Fixpoint collect_hdrs_prog (prog : P4c.TopDecl.d tags_t) : list (H * nat) :=
   match prog with
-  | TPParser _ _ _ _ start states _ => []
+  | TopDecl.TPParser _ _ _ _ start states _ => []
   | TPSeq d1 d2 _ => (collect_hdrs_prog d1) ++ (collect_hdrs_prog d2)
   | _ => []
   end.
@@ -56,4 +56,4 @@ Definition mk_st (parser: P4cubparser) : Type.
 Admitted.
 
 Definition mk_states (parser: P4cubparser): mk_st parser -> state (mk_st parser) (mk_hdr_sz parser).
-Admitted. *)
+Admitted. *) *)

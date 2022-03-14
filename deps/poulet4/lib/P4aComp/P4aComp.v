21e3f8a964439b452a9bfeb79e81e3dc66526958
Calvin-S <19calvins@alumni.tas.tw>
2022-03-14 13:20:59 -0400

    Leapfrog Compiler

--- deps/poulet4/lib/P4aComp/P4aComp.v
+++ deps/poulet4/lib/P4aComp/P4aComp.v
@@ -1,19 +1,13 @@
-(* Require Export Poulet4.P4automata.Syntax.
-(* Require Export Poulet4.P4cub.Syntax.AST. *)
-Require Import Poulet4.Utils.FinType.
-
-From Poulet4 Require Export P4cub.Syntax.AST
-     P4cub.Syntax.CubNotations
-     P4cub.Syntax.IndPrincip
-     P4cub.Syntax.Equality
-     P4cub.Syntax.InferMemberTypes
-     P4cub.Syntax.Occurs
-     P4cub.Syntax.P4Field
-     P4cub.Syntax.Substitution.
+From Leapfrog Require Import Syntax Notations Ntuple.
+From Poulet4.P4cub.Syntax Require Import Syntax P4Field.
+From Poulet4.Utils Require Import FinType P4Arith.
+Require Import Coq.ZArith.ZArith
+        Coq.micromega.Lia
+        Coq.Bool.Bool.
+Import String.
 Module P4c := AST.
 Module P4a := Syntax.
-
-(* Print P4a.H. *)
+(* Open Scope p4a. *)
 
 Section P4AComp. 
   (* State identifiers. *)
@@ -27,14 +21,109 @@ Context `{H_eq_dec: EquivDec.EqDec H eq}.
 Context `{H_finite: @Finite H _ H_eq_dec}.
 
 Variable (tags_t : Type).
+(* Want to crash if untranslatable *)
+(* Fixpoint collect_hdrs (prog: P4c.TopDecl.d tags_t) : list (H * nat) :=
+  match prog with 
+    | TopDecl.TPParser p _ eps params st states i => [] ++ []
+    | TopDecl.TPSeq d1 d2 _ => collect_hdrs d1 ++ collect_hdrs d2
+    | _ => []
+  end. *)
+  Check String.eqb.
+Fixpoint type_size (ctxt:F.fs string nat) (e:Expr.t) : option nat:=
+  match e with 
+    | Expr.TBool => Some 1
+    | Expr.TBit n => Some (N.to_nat n)
+    | Expr.TInt w => Some (Pos.to_nat w)
+    | Expr.THeader fields => 
+      List.fold_left (fun accum f => 
+        match accum, f with 
+          | (Some t1), (_, t2) => 
+            match type_size ctxt t2 with 
+              | Some field_size => Some (t1 + field_size)
+              | _ => None
+            end
+          | _, _ => None
+        end) fields (Some 0)
+    | Expr.TTuple types => None  (* ? *)
+    | Expr.TVar var_name => Field.find_value (String.eqb var_name) ctxt
+    | _ => None
+  end.
 
-Fixpoint collect_hdrs_prog (prog : P4c.TopDecl.d tags_t) : list (H * nat) :=
+Fixpoint collect_hdrs_stmt (st: P4c.Stmt.s tags_t) : list (prod string nat) :=
+  match st with 
+    | Stmt.SSeq s1 s2 _ => collect_hdrs_stmt s1 ++ collect_hdrs_stmt s2
+    | _ => []
+  end.
+
+(* Fixpoitn collect_hdrs_state (state: ) :  *)
+
+Fixpoint collect_hdrs (prog: P4c.TopDecl.d tags_t) : list (prod string nat) :=
+  match prog with 
+    | TopDecl.TPParser p _ eps params st states i => [] ++ []
+    | TopDecl.TPSeq d1 d2 _ => collect_hdrs d1 ++ collect_hdrs d2
+    | _ => []
+  end.
+
+Definition mk_hdr_type (hdrs: list (prod string nat)) : Type := Fin.t (List.length hdrs).
+
+
+
+
+
+
+
+
+
+
+
+(* Notes: F.fs is an association list *)
+(* Does not handle subparsers, for now consider only one parser *)
+
+(* Inductive mk_hdr_type (parser: P4cubparser) : Type :=
+| Hdr: forall id sz, List.In (id, sz) (collect_headers parser) -> mk_hdr_type parser.
+Definition mk_hdr_sz (parser: P4cubparser) : mk_hdr_type parser -> nat.
+Admitted. *)
+
+(* Fixpoint translate_st (s:Stmt.s tags_t): op _:= 
+  match s with 
+  | Stmt.SSkip i => OpNil _
+  | Stmt.SSeq s1 s2 i => OpNil
+  | Stmt.SBlock s => OpNil
+  | _ => OpNil
+  end. *)
+(* Fixpoint translate_state () *)
+
+(* Fixpoint translate_trans (e:Parser.e) : transition
+  match e with 
+    | PGoto st i => TGoto (translate_state st)
+    | PSelet discriminee default cases i =?>
+  end. *)
+
+Fixpoint get_parser (prog: P4c.TopDecl.d tags_t) : list (Parser.state_block tags_t) :=
+  match prog with 
+    | TopDecl.TPParser p _ eps params st states i => [st] ++ []
+    | TopDecl.TPSeq d1 d2 _ => get_parser d1 ++ get_parser d2
+    | _ => []
+  end.
+
+End P4AComp.
+
+(* Fixpoint translate_parser (prog: P4c.TopDecl.d tags_t) :  *)
+
+(* Fixpoint collect_hdrs_prog (prog : P4c.TopDecl.d tags_t) : list (H * nat) :=
   match prog with
-  | TopDecl.TPParser _ _ _ _ start states _ => []
-  | TPSeq d1 d2 _ => (collect_hdrs_prog d1) ++ (collect_hdrs_prog d2)
+  | TopDecl.TPInstantiate c_name i_name type_args cargs i => 
+  | TopDecl.TPSeq d1 d2 _ => (collect_hdrs_prog d1) ++ (collect_hdrs_prog d2)
   | _ => []
   end.
-end. 
+
+(* Function find_hdr_size () *)
+
+Function collect_hdrs (parser: P4c.Expr.ct) : list H * nat) := 
+  match parser with 
+  | _ => []
+  end. *)
+
   (* Function collect_hdrs (parser: P4c.Expr.ct) : list (H * nat) :=
     match parser with
     | 
@@ -56,4 +145,4 @@ Definition mk_st (parser: P4cubparser) : Type.
 Admitted.
 
 Definition mk_states (parser: P4cubparser): mk_st parser -> state (mk_st parser) (mk_hdr_sz parser).
-Admitted. *) *)
+Admitted. *)

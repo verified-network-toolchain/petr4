b3baceeaefd0f9a98915ccd7d8f478c170189d3f
rnp39 <rnp39@cornell.edu>
2022-03-15 11:46:18 -0700

    fixed equality & syntax auxlilary defs

--- deps/poulet4/lib/P4cub/Syntax/Auxilary.v
+++ deps/poulet4/lib/P4cub/Syntax/Auxilary.v
@@ -1,176 +1,68 @@
 Set Warnings "custom-entry-overridden,parsing".
-Require Import Coq.PArith.BinPosDef Coq.PArith.BinPos
-        Coq.ZArith.BinIntDef Coq.ZArith.BinInt
+Require Import Coq.PArith.BinPos
         Poulet4.Monads.Monad Poulet4.Monads.Option
         Coq.NArith.BinNatDef Coq.NArith.BinNat.
 From Poulet4 Require Import Utils.P4Arith
      P4cub.Syntax.AST P4cub.Syntax.CubNotations
      P4cub.Syntax.Equality.
-Import Expr TypeNotations UopNotations
-       BopNotations ExprNotations MatchkindNotations.
+Import Expr ExprNotations.
 
 Fixpoint width_of_typ (τ : t) : option nat :=
   match τ with
-  | {{ Bool }} => Some 1%nat
-  | {{ bit<w> }} => Some $ N.to_nat w
-  | {{ int<w> }} => Some $ Pos.to_nat w
-  | {{ error }}  => Some 0%nat
-  | {{ tuple ts }} =>
-    ns <<| sequence $ List.map width_of_typ ts ;;
-    List.fold_left Nat.add ns 0%nat
-  | {{ struct { fs } }}
-  | {{ hdr { fs } }} =>
-    ns <<| sequence $ List.map (fun '(_,t) => width_of_typ t) fs ;;
-    List.fold_left Nat.add ns 0%nat
-  | {{ stack fs[s] }} =>
-    ns <<| sequence $ List.map (fun '(_,t) => width_of_typ t) fs ;;
-    (Pos.to_nat s * List.fold_left Nat.add ns 0%nat)%nat
+  | TBool  => Some 1%nat
+  | TBit w => Some $ N.to_nat w
+  | TInt w => Some $ Pos.to_nat w
+  | TError => Some 0%nat
   | TVar _ => None
+  | TStruct fs _ =>
+    let^ ns := sequence $ List.map width_of_typ fs in
+    List.fold_left Nat.add ns 0%nat
   end.
 
-(** Expected result type of uop. *)
-Definition t_of_uop (op : uop) (ty : t) : option t :=
-  match op, ty with
-  | _{ ! }_                            , {{ Bool }}
-  | (_{ ~ }_ | _{ - }_)                , ({{ bit<_> }} | {{ int<_> }})
-  | (_{ setValid }_ | _{ setInValid }_), {{ hdr { _ } }}   => Some ty
-  | _{ isValid }_                      , {{ hdr { _ } }}   => Some {{ Bool }}
-  | _{ Next }_                         , {{ stack ts[_] }} => Some {{ hdr { ts } }}
-  | _{ Size }_                         , {{ stack _[_] }}  => Some $ TBit 32%N
-  | _                                  , _                 => None
+(** Syntactic type of an expression. *)
+Fixpoint t_of_e (exp: e) : t := 
+  match exp with
+  | EBool _  => TBool
+  | EError _ => TError
+  | EVar τ _
+  | ECast τ _
+  | EUop τ _ _
+  | EBop τ _ _ _
+  | EMember τ _ _       => τ
+  | (w `W _)%expr       => TBit w
+  | (w `S _)%expr       => TInt w
+  | ESlice _ hi lo      => TBit (Npos hi - Npos lo + 1)%N
+  | EStruct es None     => TStruct (List.map t_of_e es) false
+  | EStruct es (Some _) => TStruct (List.map t_of_e es) true
   end.
-
-(** Expected result type of bop. *)
-Definition t_of_bop (op : bop) (l r : t) : option t :=
-  match op, l, r with
-  | (+{ +  }+ | +{ |+| }+ | +{ -  }+ | +{ |-| }+ |
-     +{ ×  }+ | +{  &  }+ | +{ ^  }+ | +{  |  }+ |
-     +{ << }+ | +{ >>  }+ | +{ && }+ | +{  ||  }+),
-    _, _ => Some l
-  | (+{ <= }+ | +{ >= }+ | +{ < }+ | +{ > }+ | +{ == }+ | +{ != }+),
-    _, _ => Some {{ Bool }}
-  | +{ ++ }+, {{ bit<wl> }}, {{ bit<wr> }}
-    => Some $ TBit (wl + wr)%N
-  | +{ ++ }+, {{ bit<wl> }}, {{ int<wr> }}
-    => Some $ TBit (wl + Npos wr)%N
-  | +{ ++ }+, {{ int<wl> }}, {{ bit<wr> }}
-    => match wr with
-      | N0 => Some $ TInt wl
-      | Npos wr => Some $ TInt (wl + wr)
-      end
-  | +{ ++ }+, {{ int<wl> }}, {{ int<wr> }}
-    => Some $ TInt (wl + wr)%positive
-  | _, _, _ => None
-  end.
-
-Definition t_of_mem (x : String.string) (ty : t) : option t :=
-  match ty with
-  | {{ struct { ts } }}
-  | {{ hdr    { ts } }} => F.get x ts
-  | _                   => None
-  end.
-
-Definition t_of_access (ty : t) : option t :=
-  match ty with
-  | {{ stack ts[_] }} => Some {{ hdr { ts } }}
-  | _                 => None
-  end.
-
-Section TofE.
-  Context {tags_t : Type}.
-  
-  (** Syntactic type of an expression. *)
-  Fixpoint t_of_e (expr: e tags_t) : t := 
-    match expr with
-    | <{ BOOL _   @ _ }> => {{ Bool }}
-    | <{ w W  _   @ _ }> => {{ bit<w> }}
-    | <{ w S  _   @ _ }> => {{ int<w> }}
-    | <{ Var  _:ty @ _ }> => ty
-    | <{ Cast _:ty @ _ }> => ty
-    | <{ Slice _ [hi:lo] @ _ }> => TBit (Npos hi - Npos lo + 1)
-    | <{ UOP _ _   : ty @ _ }> => ty
-    | <{ BOP _ _ _ : ty @ _ }> => ty
-    | <{ tup es @ _ }> =>
-      TTuple $ List.map t_of_e es
-    | <{ struct { es } @ _ }> =>
-      TStruct $ F.map t_of_e es
-    | <{ hdr { es } valid:=_ @ _ }> => 
-      THeader $ F.map t_of_e es
-    | <{ Mem _ dot _ : ty @ _ }> => ty
-    | <{ Error     _ @ _ }> => {{ error }}
-    | <{ Stack hs:ts nextIndex:= _ @ _ }> => 
-      THeaderStack ts $ Pos.of_nat $ length hs
-    | <{ Access _[_] : ts @ _ }> => {{ hdr { ts } }}
-    end.
-End TofE.
 
 (** Restrictions on type-nesting. *)
 Module ProperType.
-  Import Expr TypeNotations TypeEquivalence.
-  
   Section ProperTypeNesting.
     (** Evidence a type is a base type. *)
     Variant base_type : t -> Prop :=
-    | base_bool : base_type {{ Bool }}
-    | base_bit (w : N) : base_type {{ bit<w> }}
-    | base_int (w : positive) : base_type {{ int<w> }}.
+    | base_bool : base_type TBool
+    | base_bit (w : N) : base_type (TBit w)
+      | base_int (w : positive) : base_type (TInt w).
     
-    (** Allowed types within headers. *)
-    Variant proper_inside_header : t -> Prop :=
-    | pih_bool (τ : t) :
-        base_type τ ->
-        proper_inside_header τ
-    | pih_struct (ts : F.fs String.string t) :
-        F.predfs_data base_type ts ->
-        proper_inside_header {{ struct { ts } }}.
+    (* TODO: Allowed types within headers. *)
+    Variant proper_inside_header : t -> Prop :=.
     
-    (** Properly nested type. *)
-    Inductive proper_nesting : t -> Prop :=
-    | pn_base (τ : t) :
-        base_type τ -> proper_nesting τ
-    | pn_error : proper_nesting {{ error }}
-    | pn_struct (ts : F.fs String.string t) :
-        F.predfs_data proper_nesting ts ->
-        proper_nesting {{ struct { ts } }}
-    | pn_tuple (ts : list t) :
-        Forall proper_nesting ts ->
-        proper_nesting {{ tuple ts }}
-    | pn_header (ts : F.fs String.string t) :
-        F.predfs_data proper_inside_header ts ->
-        proper_nesting {{ hdr { ts } }}
-    | pn_header_stack (ts : F.fs String.string t)
-                      (n : positive) :
-        BitArith.bound 32 (Zpos n) ->
-        F.predfs_data proper_inside_header ts ->
-        proper_nesting {{ stack ts[n] }}.
-    
-    Import F.FieldTactics.
+    (* TODO: Properly nested type. *)
+    Inductive proper_nesting : t -> Prop :=.
     
     Lemma proper_inside_header_nesting : forall τ,
         proper_inside_header τ ->
         proper_nesting τ.
     Proof.
-      intros τ H. induction H.
-      - inv H; repeat econstructor.
-      - apply pn_struct.
-        ind_predfs_data; constructor; auto; cbv.
-        inv H; try (repeat constructor; assumption).
-    Qed.
+    Admitted.
   End ProperTypeNesting.
   
-  Ltac invert_base_ludicrous :=
-    match goal with
-    | H: base_type {{ tuple _ }} |- _ => inv H
-    | H: base_type {{ struct { _ } }} |- _ => inv H
-    | H: base_type {{ hdr { _ } }} |- _ => inv H
-    | H: base_type {{ stack _[_] }} |- _ => inv H
-    end.
-  (**[]*)
+  (*Ltac invert_base_ludicrous :=.*)
   
   Ltac invert_proper_nesting :=
     match goal with
     | H: proper_nesting _
-      |- _ => inv H; try invert_base_ludicrous
+      |- _ => inv H(*; try invert_base_ludicrous*)
     end.
-  (**[]*)
 End ProperType.

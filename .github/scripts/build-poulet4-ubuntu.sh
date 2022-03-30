bd32fae56621dee15d145e838555d2145e07d60d
Ryan Doenges <me@ryandoeng.es>
2022-03-30 10:15:05 -0400

    Fix Core dependency issue (#344)
    * install p4pp from opam instead of pinning
    
    * attempt simplification of install
    
    * try using opam file
    
    * opam pin --no-action
    
    * more dependency fiddling
    
    * wip
    
    * fix?
    
    * fix macos version
    
    * clean up scripts

--- .github/scripts/build-poulet4-ubuntu.sh
+++ .github/scripts/build-poulet4-ubuntu.sh
@@ -9,51 +9,18 @@ set -x  # Make command execution verbose
 export PETR4_DEPS="m4 \
                    libgmp-dev"
 
-export PETR4_DEPS_OPAM="ANSITerminal \
-                        alcotest \
-                        bignum \
-                        cstruct-sexp \
-                        pp \
-                        ppx_deriving \
-                        ppx_deriving_yojson \
-                        yojson \
-                        js_of_ocaml \
-                        js_of_ocaml-lwt \
-                        js_of_ocaml-ppx"
-
-export POULET4_DEPS="coq-equations \
-                     coq-record-update \
-                     coq-compcert "
-
-export POULET4_CCOMP_DEPS="zarith"
-
-# install deps
+# install dependencies
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends \
-  ${PETR4_DEPS}
-opam update
-opam upgrade
-# install p4pp
-opam switch 4.12.0
-opam install menhir.20211128
-opam pin add p4pp https://github.com/cornell-netlab/p4pp.git
-opam pin add coq-vst-zlist https://github.com/PrincetonUniversity/VST.git
+sudo apt-get install -y --no-install-recommends ${PETR4_DEPS}
+opam switch 4.14.0
 eval $(opam env)
-
-# install deps for poulet4
-opam pin add coq 8.13.2
 opam repo add coq-released https://coq.inria.fr/opam/released
-# install dependencies for petr4, poulet4, poulet4_ccomp
-opam install \
-  ${PETR4_DEPS_OPAM} \
-  ${POULET4_DEPS} \
-  ${POULET4_CCOMP_DEPS}
-#opam install coq-equations coq-record-update coq-compcert 
-# install deps for poulet4_ccomp
-#opam install zarith
+opam update
 
-#dune external-lib-deps --missing @install
-opam install ppx_import
+opam pin add --no-action p4pp https://github.com/cornell-netlab/p4pp.git
+opam pin add --no-action coq-vst-zlist https://github.com/PrincetonUniversity/VST.git
+
+opam install . deps/poulet4 deps/poulet4_Ccomp/extraction --deps-only
 
 make
 make install

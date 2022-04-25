98a344f31768e1490b6af7681ef62dd28cd909fd
Ryan Doenges <me@ryandoeng.es>
2022-04-25 18:55:10 -0400

    a couple more examples + rewrite rule

--- deps/table_opt/src/main.rs
+++ deps/table_opt/src/main.rs
@@ -1,43 +1,66 @@
 use egg::{*, rewrite as rw};
 
+// From https://github.com/egraphs-good/egg/blob/main/tests/lambda.rs
+fn var(s: &str) -> Var {
+    s.parse().unwrap()
+}
+
+// From https://github.com/egraphs-good/egg/blob/main/tests/lambda.rs
+fn is_not_same_var(v1: Var, v2: Var) -> impl Fn(&mut EGraph<SymbolLang, ()>, Id, &Subst) -> bool {
+    move |egraph, _, subst| egraph.find(subst[v1]) != egraph.find(subst[v2])
+}
+
+fn run(rules: &[Rewrite<SymbolLang, ()>], orig_str: &str) {
+    let orig_expr = orig_str.parse().unwrap();
+    let runner = Runner::default().with_expr(&orig_expr).run(rules);
+    let extractor = Extractor::new(&runner.egraph, AstSize);
+    let (best_cost, best_expr) = extractor.find_best(runner.roots[0]);
+
+    print!("orig: {}\nbest: {}\ncost: {}\n\n", orig_expr, best_expr, best_cost);
+}
+
 fn main() {
     let rules: &[Rewrite<SymbolLang, ()>] = &[
-        rw!("if-same"; "(if ?b ?x ?x)" => "?x"),
-        rw!("distr-seq-if"; "(seq (if ?b ?x ?y) ?z)" => "(if ?b (seq ?x ?z) (seq ?y ?z))"),
-        rw!("set-test-eq"; "(seq (set ?x ?e) (if (eq ?x ?e) ?t ?f))" => "(seq (set ?x ?e) ?t)"),
-        //rw!("set-test-neq"; "(seq (set ?x ?e1) (if (eq ?x ?e2) ?t ?f))" =>
-        //    "(seq (set ?x ?e1) ?f)"
-        //if ConditionEqual::parse("?e1", "?e2")),
+        rw!("if-same"; "(if ?b ?x ?x)" =>
+                       "?x"),
+        rw!("distr-seq-if"; "(seq (if ?b ?x ?y) ?z)" =>
+                            "(if ?b (seq ?x ?z) (seq ?y ?z))"),
+        rw!("distr-seq-if-rev"; "(if ?b (seq ?x ?z) (seq ?y ?z))" =>
+                                "(seq (if ?b ?x ?y) ?z)"),
+        rw!("set-test-eq"; "(seq (set ?x ?e) (if (eq ?x ?e) ?t ?f))" =>
+                           "(seq (set ?x ?e) ?t)"),
+        rw!("set-test-neq"; "(seq (set ?x ?e1) (if (eq ?x ?e2) ?t ?f))" =>
+                            "(seq (set ?x ?e1) ?f)"
+                            if is_not_same_var(var("?e1"), var("?e2"))),
         rw!("test-comm"; "(eq ?x ?y)" => "(eq ?y ?x)"),
         rw!("seq-id-l"; "(seq nop ?x)" => "?x"),
         rw!("seq-id-r"; "(seq ?x nop)" => "?x"),
     ];
 
-    // While it may look like we are working with numbers,
-    // SymbolLang stores everything as strings.
-    // We can make our own Language later to work with other types.
-    let start =
-"
-(seq
-  (if (eq ip 10.0.0.1)
-      (set eth aa:bb:cc:dd:ee:ff)
-      (set eth aa:aa:aa:aa:aa:aa))
-  (if (eq eth aa:bb:cc:dd:ee:ff)
-      (set port 2)
-      (set port 1)))
-".parse().unwrap();
+    run(rules, "(seq
+                   (if (eq ip 10.0.0.1)
+                       (set eth aa:bb:cc:dd:ee:ff)
+                       (set eth aa:aa:aa:aa:aa:aa))
+                   (if (eq eth aa:bb:cc:dd:ee:ff)
+                       (set port 1)
+                       (set port 2)))");
 
-    // That's it! We can run equality saturation now.
-    let runner = Runner::default().with_expr(&start).run(rules);
+    run(rules, "(seq
+                   (if cond1 (set eth A)
+                             (if cond2
+                                 (set eth C)
+                                 (set eth D)))
+                   (if (eq eth B)
+                       (set port 1)
+                       (set port 2)))");
 
-    // Extractors can take a user-defined cost function,
-    // we'll use the egg-provided AstSize for now
-    let extractor = Extractor::new(&runner.egraph, AstSize);
-
-    // We want to extract the best expression represented in the
-    // same e-class as our initial expression, not from the whole e-graph.
-    // Luckily the runner stores the eclass Id where we put the initial expression.
-    let (best_cost, best_expr) = extractor.find_best(runner.roots[0]);
-
-    println!("best expression: {}\ncost: {}", best_expr, best_cost);
+    run(rules, "(seq
+                   (if (eq ip 10.0.0.1)
+                       (set eth A)
+                       (if (eq ip 10.0.0.2)
+                           (set eth B)
+                           (set eth F)))
+                   (if (eq eth A)
+                       (set port 1)
+                       (set port 2)))");
 }

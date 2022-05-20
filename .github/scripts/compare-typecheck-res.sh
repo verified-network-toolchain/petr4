634f855be1102c382f96822fd38f50e40e746595
pataei <paris.ataei@gmail.com>
2022-05-20 00:07:10 -0400

    fix dir

--- .github/scripts/compare-typecheck-res.sh
+++ .github/scripts/compare-typecheck-res.sh
@@ -7,10 +7,10 @@ err=0
 for file in $(find /petr4/ci-test/type-checking/expectation/fails -name '*.p4')
 do
   echo "type checking ${file} with petr4..."
-  petr4_type=$(petr4 typecheck -I /petr4/ci-test/testdata/p4include "$file" 2>&1)
+  petr4_type=$(petr4 typecheck -I /petr4/ci-test/p4include "$file" 2>&1)
   petr4_type_stat=$?
   echo "type checking ${file} with p4c..."
-  p4c_type=$(p4test -I /petr4/ci-test/testdata/p4include "$file" 2>&1)
+  p4c_type=$(p4test -I /petr4/ci-test/p4include "$file" 2>&1)
   p4c_type_stat=$?
   if [ $petr4_type_stat != 0 -a $p4c_type_stat != 0 ]
   then echo "PASSED!"
@@ -22,10 +22,10 @@ done
 for file in $(find /petr4/ci-test/type-checking/expectation/p4cTypeChecked -name '*.p4')
 do
     echo "type checking ${file} with petr4..."
-    petr4_type=$(petr4 typecheck -I /petr4/ci-test/testdata/p4include "$file" 2>&1)
+    petr4_type=$(petr4 typecheck -I /petr4/ci-test/p4include "$file" 2>&1)
     petr4_type_stat=$?
     echo "type checking ${file} with p4c..."
-    p4c_type=$(p4test -I /petr4/ci-test/testdata/p4include "$file" 2>&1)
+    p4c_type=$(p4test -I /petr4/ci-test/p4include "$file" 2>&1)
     p4c_type_stat=$?
     if [ $petr4_type_stat != 0 -a $p4c_type_stat = 0 ]
     then echo "PASSED!"
@@ -37,10 +37,10 @@ done
 for file in $(find /petr4/ci-test/type-checking/expectation/petr4TypeChecked -name '*.p4')
 do
     echo "type checking ${file} with petr4..."
-    petr4_type=$(petr4 typecheck -I /petr4/ci-test/testdata/p4include "$file" 2>&1)
+    petr4_type=$(petr4 typecheck -I /petr4/ci-test/p4include "$file" 2>&1)
     petr4_type_stat=$?
     echo "type checking ${file} with p4c..."
-    p4c_type=$(p4test -I /petr4/ci-test/testdata/p4include "$file" 2>&1)
+    p4c_type=$(p4test -I /petr4/ci-test/p4include "$file" 2>&1)
     p4c_type_stat=$?
     if [ $petr4_type_stat = 0 -a $p4c_type_stat != 0 ]
     then echo "PASSED!"
@@ -52,10 +52,10 @@ done
 for file in $(find /petr4/ci-test/type-checking/expectation/typechecked -name '*.p4')
 do
     echo "type checking ${file} with petr4..."
-    petr4_type=$(petr4 typecheck -I /petr4/ci-test/testdata/p4include "$file" 2>&1)
+    petr4_type=$(petr4 typecheck -I /petr4/ci-test/p4include "$file" 2>&1)
     petr4_type_stat=$?
     echo "type checking ${file} with p4c..."
-    p4c_type=$(p4test -I /petr4/ci-test/testdata/p4include "$file" 2>&1)
+    p4c_type=$(p4test -I /petr4/ci-test/p4include "$file" 2>&1)
     p4c_type_stat=$?
     if [ $petr4_type_stat = 0 -a $p4c_type_stat = 0 ]
     then echo "PASSED!"

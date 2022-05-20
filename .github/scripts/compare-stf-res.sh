8581602294b3231f40bdbbedf76443680449ea2b
pataei <paris.ataei@gmail.com>
2022-05-20 00:29:23 -0400

    detailed msg

--- .github/scripts/compare-stf-res.sh
+++ .github/scripts/compare-stf-res.sh
@@ -6,15 +6,15 @@ for file in $(find /petr4/ci-test/stf-test/expectation/fails -name '*.p4')
 do
   file1="${file%.*}"
   stf_file="${file1}.stf"
-  echo "stf testing ${file} with petr4..."
   petr4_res=$(petr4 stf -I /petr4/ci-test/p4include -stf "$stf_file" "$file" 2>&1)
   petr4_res_stat=$?
-  echo "stf testing ${file} with p4c..."
   p4c_res=$(python3 /petr4/p4c_manual/backends/bmv2/run-bmv2-test.py . -v -b -tf "$stf_file" -bd /usr/local/bin/ "$file" 2>&1)
   p4c_res_stat=$?
   if [ $petr4_res_stat != 0 -a $p4c_res_stat != 0 ]
-  then echo "PASSED!"
-  else echo "lable didn't match for ${file}! label was fails originally."
+  then echo "${file} PASSED!"
+  else echo "type checking ${file} with petr4 resulted in ${petr4_res_stat}"
+       echo "type checking ${file} with p4c resulted in ${p4c_res_stat}"
+       echo "lable didn't match for ${file}! label was fails originally."
        err=1
   fi
 done
@@ -23,15 +23,15 @@ for file in $(find /petr4/ci-test/stf-test/expectation/p4cPassed -name '*.p4')
 do
     file1="${file%.*}"
     stf_file="${file1}.stf"
-    echo "stf testing ${file} with petr4..."
     petr4_res=$(petr4 stf -I /petr4/ci-test/p4include -stf "$stf_file" "$file" 2>&1)
     petr4_res_stat=$?
-    echo "stf testing ${file} with p4c..."
     p4c_res=$(python3 /petr4/p4c_manual/backends/bmv2/run-bmv2-test.py . -v -b -tf "$stf_file" -bd /usr/local/bin/ "$file" 2>&1)
     p4c_res_stat=$?
     if [ $petr4_res_stat != 0 -a $p4c_res_stat = 0 ]
-    then echo "PASSED!"
-    else echo "lable didn't match for ${file}! label was p4cPassed originally."
+    then echo "${file} PASSED!"
+    else echo "type checking ${file} with petr4 resulted in ${petr4_res_stat}"
+         echo "type checking ${file} with p4c resulted in ${p4c_res_stat}"
+         echo "lable didn't match for ${file}! label was p4cPassed originally."
          err=1
     fi
 done
@@ -40,15 +40,15 @@ for file in $(find /petr4/ci-test/stf-test/expectation/petr4Passed -name '*.p4')
 do
     file1="${file%.*}"
     stf_file="${file1}.stf"
-    echo "stf testing ${file} with petr4..."
     petr4_res=$(petr4 stf -I /petr4/ci-test/p4include -stf "$stf_file" "$file" 2>&1)
     petr4_res_stat=$?
-    echo "stf testing ${file} with p4c..."
     p4c_res=$(python3 /petr4/p4c_manual/backends/bmv2/run-bmv2-test.py . -v -b -tf "$stf_file" -bd /usr/local/bin/ "$file" 2>&1)
     p4c_res_stat=$?
     if [ $petr4_res_stat = 0 -a $p4c_res_stat != 0 ]
-    then echo "PASSED!"
-    else echo "lable didn't match for ${file}! label was petr4Passed originally."
+    then echo "${file} PASSED!"
+    else echo "type checking ${file} with petr4 resulted in ${petr4_res_stat}"
+         echo "type checking ${file} with p4c resulted in ${p4c_res_stat}"
+         echo "lable didn't match for ${file}! label was petr4Passed originally."
          err=1
     fi
 done
@@ -57,15 +57,15 @@ for file in $(find /petr4/ci-test/stf-test/expectation/passes -name '*.p4')
 do
     file1="${file%.*}"
     stf_file="${file1}.stf"
-    echo "stf testing ${file} with petr4..."
     petr4_res=$(petr4 stf -I /petr4/ci-test/p4include -stf "$stf_file" "$file" 2>&1)
     petr4_res_stat=$?
-    echo "stf testing ${file} with p4c..."
     p4c_res=$(python3 /petr4/p4c_manual/backends/bmv2/run-bmv2-test.py . -v -b -tf "$stf_file" -bd /usr/local/bin/ "$file" 2>&1)
     p4c_res_stat=$?
     if [ $petr4_res_stat = 0 -a $p4c_res_stat = 0 ]
-    then echo "PASSED!"
-    else echo "lable didn't match for ${file}! label was passes originally."
+    then echo "${file} PASSED!"
+    else echo "type checking ${file} with petr4 resulted in ${petr4_res_stat}"
+         echo "type checking ${file} with p4c resulted in ${p4c_res_stat}"
+         echo "lable didn't match for ${file}! label was passes originally."
          err=1
     fi
 done
--- .github/scripts/compare-typecheck-res.sh
+++ .github/scripts/compare-typecheck-res.sh
@@ -6,60 +6,60 @@ err=0
 
 for file in $(find /petr4/ci-test/type-checking/expectation/fails -name '*.p4')
 do
-  echo "type checking ${file} with petr4..."
   petr4_type=$(petr4 typecheck -I /petr4/ci-test/p4include "$file" 2>&1)
   petr4_type_stat=$?
-  echo "type checking ${file} with p4c..."
   p4c_type=$(p4test -I /petr4/ci-test/p4include "$file" 2>&1)
   p4c_type_stat=$?
   if [ $petr4_type_stat != 0 -a $p4c_type_stat != 0 ]
-  then echo "PASSED!"
-  else echo "lable didn't match for ${file}! label was fails originally."
+  then echo "${file} PASSED!"
+  else echo "type checking ${file} with petr4 resulted in ${petr4_typ_stat}"
+       echo "type checking ${file} with p4c resulted in ${p4c_typ_stat}"
+       echo "lable didn't match for ${file}! label was fails originally."
        err=1
   fi
 done
 
 for file in $(find /petr4/ci-test/type-checking/expectation/p4cTypeChecked -name '*.p4')
 do
-    echo "type checking ${file} with petr4..."
     petr4_type=$(petr4 typecheck -I /petr4/ci-test/p4include "$file" 2>&1)
     petr4_type_stat=$?
-    echo "type checking ${file} with p4c..."
     p4c_type=$(p4test -I /petr4/ci-test/p4include "$file" 2>&1)
     p4c_type_stat=$?
     if [ $petr4_type_stat != 0 -a $p4c_type_stat = 0 ]
-    then echo "PASSED!"
-    else echo "lable didn't match for ${file}! label was p4cTypeChecked originally."
+    then echo "${file} PASSED!"
+    else echo "type checking ${file} with petr4 resulted in ${petr4_typ_stat}"
+         echo "type checking ${file} with p4c resulted in ${p4c_typ_stat}"
+         echo "lable didn't match for ${file}! label was p4cTypeChecked originally."
          err=1
     fi
 done
 
 for file in $(find /petr4/ci-test/type-checking/expectation/petr4TypeChecked -name '*.p4')
 do
-    echo "type checking ${file} with petr4..."
     petr4_type=$(petr4 typecheck -I /petr4/ci-test/p4include "$file" 2>&1)
     petr4_type_stat=$?
-    echo "type checking ${file} with p4c..."
     p4c_type=$(p4test -I /petr4/ci-test/p4include "$file" 2>&1)
     p4c_type_stat=$?
     if [ $petr4_type_stat = 0 -a $p4c_type_stat != 0 ]
-    then echo "PASSED!"
-    else echo "lable didn't match for ${file}! label was petr4TypeChecked originally."
+    then echo "${file} PASSED!"
+    else echo "type checking ${file} with petr4 resulted in ${petr4_typ_stat}"
+         echo "type checking ${file} with p4c resulted in ${p4c_typ_stat}"
+         echo "lable didn't match for ${file}! label was petr4TypeChecked originally."
          err=1
     fi
 done
 
 for file in $(find /petr4/ci-test/type-checking/expectation/typechecked -name '*.p4')
 do
-    echo "type checking ${file} with petr4..."
     petr4_type=$(petr4 typecheck -I /petr4/ci-test/p4include "$file" 2>&1)
     petr4_type_stat=$?
-    echo "type checking ${file} with p4c..."
     p4c_type=$(p4test -I /petr4/ci-test/p4include "$file" 2>&1)
     p4c_type_stat=$?
     if [ $petr4_type_stat = 0 -a $p4c_type_stat = 0 ]
-    then echo "PASSED!"
-    else echo "lable didn't match for ${file}! label was typechecked originally."
+    then echo "${file} PASSED!"
+    else echo "type checking ${file} with petr4 resulted in ${petr4_typ_stat}"
+         echo "type checking ${file} with p4c resulted in ${p4c_typ_stat}"
+         echo "lable didn't match for ${file}! label was typechecked originally."
          err=1
     fi
 done

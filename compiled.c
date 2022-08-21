fd41aed7ef950a28584e1a7926234798ebdc8305
rnp39 <rnp39@cornell.edu>
2022-08-21 14:20:03 -0700

    more tests left to fix

--- compiled.c
+++ compiled.c
@@ -1,5 +1,10 @@
 #include "Petr4Runtime.h"
+struct _p_e_t_r_4_0b10;
 struct _p_e_t_r_4_0b1;
+struct _p_e_t_r_4_0b10 {
+  _Bool _p_e_t_r_4_0b11;
+};
+
 struct _p_e_t_r_4_0b1 {
 };
 
@@ -10,5 +15,5 @@ void dummy_main(void)
 }
 
 
-typedef struct _p_e_t_r_4_0b1 H; 
+typedef struct _p_e_t_r_4_0b10 H; 
 typedef struct _p_e_t_r_4_0b1 M; 

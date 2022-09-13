63feb3a23b497618cd099febbc2739c0269cb228
rnp39 <rnp39@cornell.edu>
2022-09-13 10:10:34 -0700

    stmt big step preserves env length

--- deps/poulet4/lib/P4light/Semantics/Interpreter.v
+++ deps/poulet4/lib/P4light/Semantics/Interpreter.v
@@ -3,7 +3,7 @@ From Coq Require Import Strings.String
 From Poulet4 Require Import Utils.Utils
      P4light.Syntax.Syntax P4light.Syntax.Typed
      Utils.P4Arith P4light.Syntax.P4String
-     P4light.Semantics.Semantics
+     P4light.Semantics.Semantics P4light.Semantics.Ops
      Monads.Option Utils.AListUtil.
 From Equations Require Import Equations.
 Import List.ListNotations.

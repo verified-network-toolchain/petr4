ebb6be202b9d585e159bf72c8a5c732a99d7b80e
Zak Kent <zak33@cornell.edu>
2022-10-18 17:26:49 -0400

    try --no-checksums switch

--- .github/scripts/build-poulet4-ubuntu.sh
+++ .github/scripts/build-poulet4-ubuntu.sh
@@ -10,7 +10,6 @@ export PETR4_DEPS="m4 \
                    libgmp-dev"
 
 # install dependencies
-wget --no-cache https://github.com/janestreet/core_unix/archive/refs/tags/v0.15.1.tar.gz
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends ${PETR4_DEPS}
 opam switch 4.14.0
@@ -21,7 +20,7 @@ opam update
 opam pin add --no-action p4pp https://github.com/cornell-netlab/p4pp.git
 opam pin add --no-action coq-vst-zlist https://github.com/PrincetonUniversity/VST.git
 
-opam install . deps/poulet4 deps/poulet4_Ccomp/extraction --deps-only
+opam install . deps/poulet4 deps/poulet4_Ccomp/extraction --deps-only --no-checksums
 
 make
 make install

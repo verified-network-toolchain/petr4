931a52b57d369f3e078c63be55330b9a1998aedf
Eric Campbell <1234goal@gmail.com>
2023-05-10 11:27:21 -0400

    remove references to InferMemberTypes.v

--- coq/lib/P4cub/Syntax/Syntax.v
+++ coq/lib/P4cub/Syntax/Syntax.v
@@ -3,6 +3,5 @@ From Poulet4 Require Export P4cub.Syntax.AST
      P4cub.Syntax.IndPrincip
      P4cub.Syntax.Equality
      P4cub.Syntax.Auxiliary
-     P4cub.Syntax.InferMemberTypes
      P4cub.Syntax.P4Field
      P4cub.Syntax.Substitution.

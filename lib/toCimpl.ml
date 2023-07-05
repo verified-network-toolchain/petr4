f65d51546230ceb232d96a28a9d4da6024bf7881
Nate Foster <jnfoster@cs.cornell.edu>
2023-07-05 01:37:12 -0400

    Skeleton for GCL to C compiler

--- lib/toCimpl.ml
+++ lib/toCimpl.ml
@@ -1,2 +1,20 @@
 open Core
-let compile_program _ = Ok []
+open Poulet4
+open GCL
+   
+let compile_program (prsr,pipe) =
+  let dummy = [] in
+  let rec compile g = 
+    match pipe with
+    | GCL.GSkip -> []
+    | GCL.GAssign(typ,lhs,rhs) -> dummy
+    | GCL.GSeq(g1,g2) ->
+       compile g1 @
+       compile g2
+    | GCL.GChoice(g1,g2) -> dummy
+    | GCL.GAssume(phi) -> dummy
+    | GCL.GAssert(phi) -> dummy
+    | GCL.GExternVoid(e,args) -> dummy
+    | GCL.GExternAssn(x,e,args) -> dummy
+    | GCL.GTable(tbl,keys,actions) -> dummy in
+  Ok (compile pipe)

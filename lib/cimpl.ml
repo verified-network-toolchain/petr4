07690a8fdeadc0812e8aa6a88d54585432792c51
akanksha <as2637@cornell.edu>
2023-07-19 14:13:59 -0400

    Added more functionality

--- lib/cimpl.ml
+++ lib/cimpl.ml
@@ -3,14 +3,41 @@ type ctyp =
   | CTInt
 
 type cvar = string
+
+type bop = 
+(*rational operators*)
+  | EQ (*equal*)
+  | NE (*not equal*)
+  | LT (*less than*)
+  | GT (*greater than*)
+  | GTE (*greater than equal*)
+  | LTE (*less than equal*)
+(*logical operators*)
+  | And  
+  | Or
+(*arithmetic operators*)
+  | Add 
+  | Sub 
+  | Mul 
+  | Div 
+  | Mod 
   
+type uop = 
+  | Neg
+
 type cexpr =
   | CEVar of cvar
+  (*no booleans so using 1 -> TRUE and 0 -> FALSE*)
   | CEInt of int
+  | CCompExpr of bop * cexpr * cexpr
+  | CUniExpr of uop * cexpr
+  (*change to statement*)
 
 type cstmt =
   | CSSkip
   | CSAssign of cvar * cexpr
+  (* the cexpr in csif is ccompexpr -> with logical operators*)
+  | CSIf of cexpr * cstmt list * cstmt list
 
 and cblk =
   | CBlock of cstmt list
@@ -20,3 +47,4 @@ type cdecl =
                  
 type cprog =
   | CProgram of cdecl list 
+

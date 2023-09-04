5a00b734c1787281d85c9010edcfc3ecd42db46c
Nate Foster <jnfoster@cs.cornell.edu>
2023-09-04 16:49:08 +0200

    Move C Compiler to P4Cub rather than GCL

--- lib/toCimpl.ml
+++ lib/toCimpl.ml
@@ -1,6 +1,34 @@
-open Core
 open Poulet4
 open Cimpl
+open Core
 
-let compile_program (decls:ToP4cub.coq_DeclCtx) : (cprog,string) result = 
+let name_of_top (t : AST.Top.t) : string =
+  match t with
+  | Instantiate (_,n,_,_,_) -> n
+  | Extern (n,_,_,_,_) -> n
+  | Control (n,_,_,_,_,_,_) -> n
+  | Parser (n,_,_,_,_,_,_) -> n
+  | Funct (n,_,_,_) -> n
+
+let is_instantiate (t:AST.Top.t) : bool =
+  match t with
+  | Instantiate _ -> true
+  | Extern _ -> false
+  | Control _ -> false
+  | Parser _ -> false
+  | Funct _ -> false
+     
+let get_v1model_instances (prog:ToP4cub.coq_DeclCtx) =
+  let f t = is_instantiate t && String.equal (name_of_top t) "main" in
+  match List.find prog.packages ~f with
+  | Some Instantiate(_,_,_,_,[p;v;i;e;u;d]) ->
+     (p,v,i,e,u,d)
+  | Some _ ->
+     failwith "Unexpected error: main has wrong number of arguments"
+  | None ->
+     failwith "Unexpected error: main not found"
+   
+let compile_program (prog:ToP4cub.coq_DeclCtx) : (cprog,string) result = 
+  let p,v,i,e,u,d = get_v1model_instances prog in
+  let _ = ignore (p,v,i,e,u,d) in 
   Ok (Cimpl.CProgram [])
